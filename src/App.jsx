ceaf1778f5c141ab3cf23d7d60992e742299e5b9
Edgar Perez <edgarp@rockitdata.com>
2020-04-04 17:08:57 -0600

    Adding component Placeholders

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,17 @@ import { BrowserRouter, Switch, Route, Link, Redirect } from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
 import { LOGIN, PROPOSALS } from './routes';
 
-const Login = () => <div>Login Page :D</div>;
-
-const Proposals = () => <div>Proposals Page :)</div>;
-
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <ul>
-          <li>
-            <Link to={LOGIN}>Public Page</Link>
-          </li>
-          <li>
-            <Link to={PROPOSALS}>Protected Page</Link>
-          </li>
-        </ul>
-        <Switch>
-          <Route path={LOGIN} component={Login} />
-          <PrivateRoute isAuthenticated={false}>
-            <Proposals />
-          </PrivateRoute>
-          <Redirect to="/" />
-        </Switch>
-      </div>
+      <Switch>
+        {/* TODO: Add Login Component */}
+        {/* <Route path={LOGIN} component={Login} /> */}
+        <PrivateRoute isAuthenticated={false}>
+          {/* TODO: Add here Proposals component */}
+        </PrivateRoute>
+        <Redirect to="/" />
+      </Switch>
     </BrowserRouter>
   );
 };
