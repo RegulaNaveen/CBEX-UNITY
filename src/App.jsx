4b0b5c19fbaacd35729cc6e22118d18f855dfda7
Edgar Perez <edgarp@rockitdata.com>
2020-04-04 00:01:46 -0600

    Adding react router config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
 // @flow
 import React from 'react';
+import { BrowserRouter, Switch, Route, Link, Redirect } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
+import { LOGIN, PROPOSALS } from './routes';
+
+const Login = () => (
+    <div>
+        Login Page :D
+    </div>
+);
+
+const Proposals = () => (
+    <div>
+        Proposals Page :)
+    </div>
+)
 
 const App = () => {
-    return(
-        <div>
-            Hello world :D
-        </div>
+    return (
+        <BrowserRouter>
+            <div>
+                <ul>
+                    <li>
+                        <Link to={LOGIN}>Public Page</Link>
+                    </li>
+                    <li>
+                        <Link to={PROPOSALS}>Protected Page</Link>
+                    </li>
+                </ul>
+                <Switch>
+                    <Route path={LOGIN} component={Login} />
+                    <PrivateRoute authenticated path={PROPOSALS}>
+                        <Proposals />
+                    </PrivateRoute>
+                    <Redirect to="/" />
+                </Switch>
+            </div>
+        </BrowserRouter>
     )
 }
 
---
+++ src/PrivateRoute.jsx
@@ -0,0 +1,32 @@
+// @flow
+import React, { PureComponent } from 'react';
+import type { Node } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { LOGIN } from './routes';
+
+type Props = {
+    children: Node,
+    authenticated: boolean
+};
+
+class PrivateRoute extends PureComponent<Props> {
+    renderRoute = () => {
+        const { authenticated, children } = this.props;
+        return authenticated ? (
+            children
+        ) : (
+                <Redirect
+                    to={LOGIN}
+                />
+            )
+    }
+
+    render() {
+        return (
+            <Route render={this.renderRoute}/>
+        )
+    }
+}
+
+
+export default PrivateRoute;
