50ee7d752347fdd06ab581df6f831588e52301ba
Laura Gallardo <laura@cbqasolutions.com>
2020-04-06 11:04:09 -0500

    Fixing rebase

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import React from 'react';
 import { BrowserRouter, Switch, Route, Link, Redirect } from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
 import { LOGIN, PROPOSALS } from './routes';
+import Login from './components/auth/Login';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
         {/* TODO: Add Login Component */}
-        {/* <Route path={LOGIN} component={Login} /> */}
+        <Route path={LOGIN} component={Login} />
         <PrivateRoute isAuthenticated={false}>
           {/* TODO: Add here Proposals component */}
         </PrivateRoute>
-        <Redirect to="/" />
+        <Redirect to='/' />
       </Switch>
     </BrowserRouter>
   );
--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -5,22 +5,20 @@ import img from '../../../img/login-background.png';
 
 const Login = () => {
   return (
-    <div className='login-wrapper'>
-      <div className='gradient-background-img' />
-        <div>
-          <img
-            className='login-background-img'
-            src={img}
-            alt='Login background'
-            role='presentation'
-          />
-        </div>
-        <div className='login-form'>
-          <LoginForm />
-          <p className='copyright-text'>
-            Copyright @ 2019. All rights reserved
-          </p>
-        </div>
+    <div className="login-wrapper">
+      <div className="gradient-background-img" />
+      <div>
+        <img
+          className="login-background-img"
+          src={img}
+          alt="Login background"
+          role="presentation"
+        />
+      </div>
+      <div className="login-form">
+        <LoginForm />
+        <p className="copyright-text">Copyright @ 2019. All rights reserved</p>
+      </div>
     </div>
   );
 };
