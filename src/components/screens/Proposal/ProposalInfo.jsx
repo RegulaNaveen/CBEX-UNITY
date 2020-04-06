500a01527000115d9181192ae777533e33c9dbb2
Edgar Perez <edgar@cbqasolutions.com>
2020-04-06 17:50:10 -0500

    Fixing margins in proposal details component

--- src/components/screens/Proposal/ProposalInfo.jsx
+++ src/components/screens/Proposal/ProposalInfo.jsx
@@ -5,52 +5,52 @@ function ProposalInfo() {
   return (
     <div className="proposalInfo-wrapper">
       <p className="proposalInfo-title">RFP-1028</p>
-      <div className="proposalInfo-subtitle">Details</div>
-      <div className="proposalInfo-details-wrapper">
-        <div className="proposalInfo-details-column">
-          <div className="proposalInfo-details-segment">
-            <div className="proposalInfo-details-data">Account Executive:</div>
-            <div className="proposalInfo-details-data">Jan Levinson-Gould</div>
+      <p className="proposalInfo-subtitle">Details</p>
+      <div className="proposalInfo-details">
+        <div className="proposalInfo-details-row">
+          <div className="proposalInfo-details-column">
+            <p className="proposalInfo-details-title">Account Executive:</p>
+            <p className="proposalInfo-details-subtitle">Jan Levinson-Gould</p>
           </div>
-          <div className="proposalInfo-details-segment">
+          <div className="proposalInfo-details-column">
             <div className="proposalInfo-details-data">
               Business Development:
             </div>
             <div className="proposalInfo-details-data">Dwight Schrute</div>
           </div>
         </div>
-        <div className="proposalInfo-details-column">
-          <div className="proposalInfo-details-segment">
+        <div className="proposalInfo-details-row">
+          <div className="proposalInfo-details-column">
             <div className="proposalInfo-details-data">Proposal Director:</div>
             <div className="proposalInfo-details-data">Michael Scott</div>
           </div>
-          <div className="proposalInfo-details-segment">
+          <div className="proposalInfo-details-column">
             <div className="proposalInfo-details-data">Labs:</div>
             <div className="proposalInfo-details-data">Kevin Malone</div>
           </div>
         </div>
-        <div className="proposalInfo-details-column">
-          <div className="proposalInfo-details-row-segment">
+        <div className="proposalInfo-details-row">
+          <div className="proposalInfo-details-column">
             <div className="proposalInfo-details-data">Synopsis Sent:</div>
             <div className="proposalInfo-details-data">Yes</div>
           </div>
-          <div className="proposalInfo-details-row-segment">
+          <div className="proposalInfo-details-column">
             <div className="proposalInfo-details-data">Phase:</div>
             <div className="proposalInfo-details-data">2</div>
           </div>
-          <div className="proposalInfo-details-row-segment">
+          <div className="proposalInfo-details-column">
             <div className="proposalInfo-details-data">Number of Sites:</div>
             <div className="proposalInfo-details-data">12</div>
           </div>
         </div>
-        <div className="proposalInfo-details-column">
-          <div className="proposalInfo-details-segment">
+        <div className="proposalInfo-details-row">
+          <div className="proposalInfo-details-column">
             <div className="proposalInfo-details-data">Countries:</div>
             <div className="proposalInfo-details-data">
               France, UK, Italy, Spain
             </div>
           </div>
-          <div className="proposalInfo-details-segment">
+          <div className="proposalInfo-details-column">
             <div className="proposalInfo-details-data">Indication:</div>
             <div className="proposalInfo-details-data">Myopia</div>
           </div>
