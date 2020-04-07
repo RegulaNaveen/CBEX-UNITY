07d18a11288f023afb3ceff517b2af05d6a500b6
CarlosR <carlos.rodriguez@cbqasolutions.com>
2020-04-07 15:33:38 -0500

    Add collapsible behavior to Task component
    Add flow notation to Task component

--- src/components/screens/Proposal/Task.jsx
+++ src/components/screens/Proposal/Task.jsx
@@ -1,8 +1,88 @@
-import React from 'react';
+// @flow
+import React, { Component } from 'react';
 
-const Task = props => {
-  const { collapsed } = props;
-  const testArray = [
+type State = {
+  collapsed: boolean
+};
+
+type Props = {
+  testArray?: Array<Object>
+};
+
+class Task extends Component<Props, State> {
+  constructor(props: Object) {
+    super(props);
+
+    this.state = {
+      collapsed: false
+    };
+  }
+
+  handleCollapse = () => {
+    const { collapsed } = this.state;
+    this.setState({ collapsed: !collapsed });
+  };
+
+  handleKeyPress = (event: KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      this.handleCollapse();
+    }
+  };
+
+  render() {
+    const { collapsed } = this.state;
+    const { testArray } = this.props;
+    return (
+      <div className="task-wrapper">
+        <div
+          className="task-icon"
+          onClick={this.handleCollapse}
+          onKeyDown={this.handleKeyPress}
+          role="button"
+          tabIndex={0}
+        >
+          {'>'}
+        </div>
+        {!collapsed ? (
+          <div className="task-title-wrapper">
+            <div className="task-title">Resources</div>
+            <div className="task-status-wrapper">
+              <div className="task-status-description">8 Incomplete</div>
+            </div>
+          </div>
+        ) : (
+          <div className="task-table-wrapper">
+            <div className="task-table-headers">
+              <div className="task-title">Resources</div>
+              <div className="task-subtitle task-subtitle-answer">Answer</div>
+              <div className="task-subtitle task-subtitle-owner">Owner</div>
+              <div className="task-subtitle task-subtitle-due-date">
+                Due Date
+              </div>
+              <div className="task-subtitle task-subtitle-completion-date">
+                Date Completed
+              </div>
+            </div>
+            {testArray.map(item => (
+              <div className="task-table-row">
+                <div className="task-table-row-question">{item.question}</div>
+                <div className="task-table-row-answer">{item.answer}</div>
+                <div className="task-table-row-owner">{item.owner}</div>
+                <div className="task-table-row-due-date">{item.dueDate}</div>
+                <div className="task-table-row-completion-date">
+                  {item.completionDate}
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+Task.defaultProps = {
+  testArray: [
     {
       question: 'Question',
       answer: 'Answer',
@@ -17,43 +97,7 @@ const Task = props => {
       dueDate: '02-Apr-2020',
       completionDate: '02-Apr-2020'
     }
-  ];
-  return (
-    <div className="task-wrapper">
-      <div className="task-icon">{'>'}</div>
-      {!collapsed ? (
-        <div className="task-title-wrapper">
-          <div className="task-title">Resources</div>
-          <div className="task-status-wrapper">
-            <div className="task-status-description">8 Incomplete</div>
-          </div>
-        </div>
-      ) : (
-        <div className="task-table-wrapper">
-          <div className="task-table-headers">
-            <div className="task-title">Resources</div>
-            <div className="task-subtitle task-subtitle-answer">Answer</div>
-            <div className="task-subtitle task-subtitle-owner">Owner</div>
-            <div className="task-subtitle task-subtitle-due-date">Due Date</div>
-            <div className="task-subtitle task-subtitle-completion-date">
-              Date Completed
-            </div>
-          </div>
-          {testArray.map(item => (
-            <div className="task-table-row">
-              <div className="task-table-row-question">{item.question}</div>
-              <div className="task-table-row-answer">{item.answer}</div>
-              <div className="task-table-row-owner">{item.owner}</div>
-              <div className="task-table-row-due-date">{item.dueDate}</div>
-              <div className="task-table-row-completion-date">
-                {item.completionDate}
-              </div>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  ]
 };
 
 export default Task;
