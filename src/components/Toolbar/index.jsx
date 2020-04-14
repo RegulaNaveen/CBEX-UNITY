f023908b0f919f18abeb8a095d9fb945e208d1de
CarlosR <carlos.rodriguez@cbqasolutions.com>
2020-04-14 16:35:59 -0500

    Add Dropdown final layout
    Add Dropdown items styles
    UT missing

--- src/components/Toolbar/index.jsx
+++ src/components/Toolbar/index.jsx
@@ -1,10 +1,12 @@
 // @flow
 import React, { Component } from 'react';
-import { Search, Bell, DropMenu } from '../svg';
+import { Search, Bell, DropMenu, User, Help, Settings } from '../svg';
 
 type State = {
   isCollapsed: boolean
 };
+
+type Props = {};
 
 class Toolbar extends Component<Props, State> {
   constructor(props: Object) {
@@ -37,28 +39,41 @@ class Toolbar extends Component<Props, State> {
           <p className="toolbar-navigation-title proposals">Proposals</p>
           <p className="toolbar-navigation-title">Q & A</p>
         </div>
-        <div className="toolbar-profile-spacer">
-          <div className="toolbar-profile-wrapper">
-            <Search className="toolbar-profile-search" />
-            <Bell className="toolbar-profile-notifications" />
+        <div className="toolbar-account-spacer">
+          <div className="toolbar-account-wrapper">
+            <Search className="toolbar-account-search" />
+            <Bell className="toolbar-account-notifications" />
             <div
-              className="toolbar-profile-info"
+              className="toolbar-account-info"
               role="button"
               onClick={this.handleCollapse}
               onKeyPress={this.handleKeyPress}
               type="button"
               tabIndex={-1}
             >
-              <p className="toolbar-profile-info-title">Oliver Queen</p>
-              <DropMenu className="toolbar-profile-info-icon" />
+              <p className="toolbar-account-info-title">Oliver Queen</p>
+              <DropMenu className="toolbar-account-info-icon" />
             </div>
             {isCollapsed ? (
-              <div className="toolbar-profile-menu">
-                <p className="toolbar-profile-menu-name">Oliver Queen</p>
-                <p className="toolbar-profile-menu-email">
+              <div className="toolbar-account-menu">
+                <p className="toolbar-account-menu-name">Oliver Queen</p>
+                <p className="toolbar-account-menu-email">
                   oliver.queen@iqvia.com
                 </p>
-                <div className="toolbar-profile-menu-separator" />
+                <div className="toolbar-account-menu-separator" />
+                <div className="toolbar-account-menu-option">
+                  <User className="toolbar-account-menu-option-icon" />
+                  <p className="toolbar-account-menu-option-title">Profile</p>
+                </div>
+                <div className="toolbar-account-menu-option">
+                  <Settings className="toolbar-account-menu-option-icon" />
+                  <p className="toolbar-account-menu-option-title">Settings</p>
+                </div>
+                <div className="toolbar-account-menu-option">
+                  <Help className="toolbar-account-menu-option-icon" />
+                  <p className="toolbar-account-menu-option-title">Help</p>
+                </div>
+                <div className="toolbar-account-menu-button">Log out</div>
               </div>
             ) : null}
           </div>
--- src/components/svg/index.js
+++ src/components/svg/index.js
@@ -5,5 +5,8 @@ import Add from './Add';
 import DropMenu from './DropMenu';
 import Bell from './Bell';
 import Search from './Search';
+import User from './User';
+import Help from './Help';
+import Settings from './Settings';
 
-export { Checkmark, Edit, Add, DropMenu, Bell, Search };
+export { Checkmark, Edit, Add, DropMenu, Bell, Search, User, Help, Settings };
