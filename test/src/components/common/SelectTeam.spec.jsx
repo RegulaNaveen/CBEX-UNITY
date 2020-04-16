db5fec00271959d94aa1a005c7b0f3a116493b77
Laura Gallardo <laura@cbqasolutions.com>
2020-04-15 19:42:30 -0500

    Adding SelectTeam tests

---
+++ test/src/components/common/SelectTeam.spec.jsx
@@ -0,0 +1,34 @@
+// @flow
+import expect from 'expect';
+import { describe, it } from 'mocha';
+import SelectTeam from './models/SelectTeamModel';
+
+describe('SelectTeam component', () => {
+  const children = 'Fake id';
+
+  describe('rendering', () => {
+    it('should render the correct children', () => {
+      const wrapper = new SelectTeam(children);
+      expect(wrapper.getChildren()).toBe(children);
+    });
+
+    it('should render the correct Paragraph className', () => {
+      const wrapper = new SelectTeam(children);
+      expect(wrapper.getTitleClassName()).toBe('selected-team-title');
+    });
+
+    it('should render Icon component', () => {
+      const wrapper = new SelectTeam(children);
+      expect(wrapper.hasIcon()).toBe(true);
+    });
+  });
+
+  describe('interactions', () => {
+    it('should handle onChange handler', () => {
+      const wrapper = new SelectTeam(children);
+      wrapper.doOnChange();
+      expect(wrapper.onChangeCalledOnce()).toBe(true);
+      wrapper.resetEventHandlers();
+    });
+  });
+});
