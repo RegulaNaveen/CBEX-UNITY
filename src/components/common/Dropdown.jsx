e1d5cbe047aea2692c7ddc9730ebbc3193571ba4
Laura Gallardo <laura@cbqasolutions.com>
2020-04-16 14:42:21 -0500

    Dropdown fixes

--- src/components/common/Dropdown.jsx
+++ src/components/common/Dropdown.jsx
@@ -2,30 +2,70 @@
 import React, { Component } from 'react';
 
 type Props = {
+  id: string,
+  placeholder: string,
   items: Array<Object>,
   title: string
 };
 
-type State = {};
+type State = {
+  isCollapsed: boolean,
+  selectedValue: string
+};
 
 class Dropdown extends Component<Props, State> {
   constructor(props: Object) {
     super(props);
-    this.state = {};
+
+    this.state = {
+      isCollapsed: false,
+      selectedValue: ''
+    };
   }
 
+  handleCollapse = () => {
+    const { isCollapsed } = this.state;
+    this.setState({ isCollapsed: !isCollapsed });
+  };
+
+  onClick = (value: string) => {
+    this.setState({ selectedValue: value }, () => {
+      this.handleCollapse();
+    });
+    console.log(value);
+  };
+
   render() {
-    const { items, title } = this.props;
+    const { isCollapsed } = this.state;
+    const { id, placeholder, items, title } = this.props;
     return (
       <>
         <p className="dd-title">{title}</p>
-        <select className="dd-list">
-          {items.map(item => (
-            <option className="dd-list-item" key={item.id}>
-              {item.title}
-            </option>
-          ))}
-        </select>
+        <div className="dd-wrapper">
+          <div
+            id={id}
+            className="dd-header"
+            role="presentation"
+            onClick={this.handleCollapse}
+          >
+            <div className="dd-header-title">{placeholder}</div>
+          </div>
+          {isCollapsed && (
+            <ul className="dd-list">
+              {items.map(item => (
+                <li
+                  role="presentation"
+                  className="dd-list-item"
+                  key={item.id}
+                  // eslint-disable-next-line react/jsx-no-bind
+                  onClick={() => this.onClick(item.title)}
+                >
+                  {item.title}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </>
     );
   }
