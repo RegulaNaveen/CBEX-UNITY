3a10025ef25067d814ba3e872f952df410f4d219
Laura Gallardo <laura@cbqasolutions.com>
2020-04-20 12:43:29 -0500

    Add Modal background

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -7,7 +7,8 @@ type Props = {
 
 const Modal = ({ children }: Props) => {
   return (
-    <div className="modal-wrapper">
+    <div className="modal-wrapper" role="dialog">
+      <div className="modal-dialog-blur" role="document" />
       <div className="modal-dialog-wrapper">{children}</div>
     </div>
   );
