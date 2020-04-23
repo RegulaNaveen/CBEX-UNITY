0c86886095c7ecaeed3c06aea35389784941370b
CarlosR <carlos.rodriguez@cbqasolutions.com>
2020-04-23 11:54:07 -0500

    Update flow version
    Add immutable to flow [libs]
    Adjust svg component props

--- src/components/svg/Close.jsx
+++ src/components/svg/Close.jsx
@@ -5,9 +5,9 @@ type Props = {
   className: string
 };
 
-function Close(props: Props) {
+function Close({ className }: Props) {
   return (
-    <svg width={12} height={12} viewBox="0 0 24 24" {...props}>
+    <svg width={12} height={12} viewBox="0 0 24 24" className={className}>
       <path
         d="M14.3 12.179a.25.25 0 010-.354l9.263-9.262A1.5 1.5 0 1021.439.442L12.177 9.7a.25.25 0 01-.354 0L2.561.442A1.5 1.5 0 10.439 2.563L9.7 11.825a.25.25 0 010 .354L.439 21.442a1.5 1.5 0 002.122 2.121l9.262-9.263a.25.25 0 01.354 0l9.262 9.263a1.5 1.5 0 102.122-2.121L14.3 12.179z"
         fill="#000"
--- src/components/svg/Search.jsx
+++ src/components/svg/Search.jsx
@@ -5,9 +5,9 @@ type Props = {
   className: string
 };
 
-const Search = (props: Props) => {
+const Search = ({ className }: Props) => {
   return (
-    <svg width={24} height={24} viewBox="0 0 24 24" {...props}>
+    <svg width={24} height={24} viewBox="0 0 24 24" className={className}>
       <path
         d="M23.384 21.619l-6.529-6.529a9.284 9.284 0 10-1.768 1.768l6.529 6.529c.492.48 1.276.48 1.768 0a1.251 1.251 0 000-1.768zM2.75 9.5a6.75 6.75 0 116.75 6.75A6.758 6.758 0 012.75 9.5z"
         fill="#fff"
