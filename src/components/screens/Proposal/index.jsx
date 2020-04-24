a7a6ceb1e0930e235a07d582f4780671435400f1
CarlosR <carlos.rodriguez@cbqasolutions.com>
2020-04-24 11:45:23 -0500

    Return showModal to proper value

--- src/components/screens/Proposal/index.jsx
+++ src/components/screens/Proposal/index.jsx
@@ -29,7 +29,7 @@ class Proposal extends Component<Props, State> {
     super(props);
 
     this.state = {
-      showModal: true,
+      showModal: false,
       data: {
         title: 'RFP-1028',
         accountExecutive: 'Jan Levinson-Gould',
