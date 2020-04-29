faaef58a6e3fbc7685f2f42f8955f8c45dbaf87c
Laura Gallardo <laura@cbqasolutions.com>
2020-04-29 16:16:11 -0500

    Adding reducer tests

--- test/src/reducers/proposal.spec.js
+++ test/src/reducers/proposal.spec.js
@@ -1,23 +1,47 @@
 // @flow
 import expect from 'expect';
 import { describe, it } from 'mocha';
-import { fromJS } from 'immutable';
-import { PROPOSAL_INFO } from '../../../src/actions/proposal-types';
+import { fromJS, Map } from 'immutable';
+import {
+  PROPOSAL_INFO,
+  PROPOSAL_INFO_LOADING,
+  PROPOSAL_INFO_ERROR
+} from '../../../src/actions/proposal-types';
 import proposalReducer from '../../../src/reducers/proposal';
 
-describe('proposal reducer', () => {
-  const initialState = fromJS({
-    proposalQuestions: undefined
+describe('Proposal', () => {
+  const initialState = fromJS({});
+
+  it('should load proposal questions', () => {
+    const proposalQuestions = ['Fake Question 1', 'Fake Question 2'];
+    const action = { type: PROPOSAL_INFO, payload: { proposalQuestions } };
+    const newState = proposalReducer(initialState, action);
+    const expectedState = Map({ proposalQuestions });
+    expect(newState).toEqual(expectedState);
   });
 
-  it('onProsalInfoLoaded', () => {
-    const proposalQuestions = Map({});
-    const action = { type: PROPOSAL_INFO, payload: proposalQuestions };
+  it('should set proposal error state', () => {
+    const errorMessage = 'Fake Error Message';
+    const action = {
+      type: PROPOSAL_INFO_ERROR,
+      payload: 'Fake Error Message'
+    };
     const newState = proposalReducer(initialState, action);
-    const expectedState = initialState.set(
-      'proposalQuestions',
-      proposalQuestions
-    );
+    const expectedState = initialState
+      .set('proposalError', errorMessage)
+      .set('isProposalLoading', false);
+    expect(newState).toEqual(expectedState);
+  });
+
+  it('should set proposal loading state', () => {
+    const action = {
+      type: PROPOSAL_INFO_LOADING,
+      payload: ''
+    };
+    const newState = proposalReducer(initialState, action);
+    const expectedState = initialState
+      .set('isProposalLoading', true)
+      .set('proposalError', undefined);
     expect(newState).toEqual(expectedState);
   });
 });
