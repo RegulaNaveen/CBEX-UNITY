03efb4e672e2c0c6d95e78153e6a0be1818fce8f
CarlosR <carlos.rodriguez@cbqasolutions.com>
2020-05-20 15:14:09 -0500

    Add functionality to order Sections

--- src/selectors/proposal.js
+++ src/selectors/proposal.js
@@ -4,14 +4,31 @@ import _ from 'lodash';
 
 const getSections = (questions: Array<Object>) => {
   const sections = [];
+  const orderedSections = [];
 
   questions.forEach((question: Object) => {
     const { section } = question;
-    if (!sections.includes(section.sectionName))
-      sections.push(section.sectionName);
+    let willAdd = true;
+
+    if (sections.length === 0) sections.push(section);
+    else {
+      for (let i = 0; i < sections.length; i += 1) {
+        if (sections[i].sectionName === section.sectionName) {
+          willAdd = false;
+          break;
+        }
+      }
+      if (willAdd) sections.push(section);
+    }
   });
 
-  return sections;
+  for (let i = 1; i < sections.length + 1; i += 1) {
+    sections.forEach((section: Object) => {
+      if (section.sectionOrder === i) orderedSections.push(section.sectionName);
+    });
+  }
+
+  return orderedSections;
 };
 
 const sortData = (questions: Array<Object>) => {
