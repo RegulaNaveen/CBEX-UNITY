3f6a8558709e1b930cb46c1dc7a316c92a74ea6e
Carlos Alberto Marún Rodríguez <carlos.marun@cbqasolutions.com>
2020-08-12 15:50:46 -0500

    Comments fixed.

--- src/components/common/ProposalCard.jsx
+++ src/components/common/ProposalCard.jsx
@@ -1,7 +1,7 @@
 // @flow
 import React from 'react';
-import '../../../styles/App.scss';
-import { Folder, Clipboard, RightArrow } from '../svg/index';
+import { v4 as uuidv4 } from 'uuid';
+import { Folder, Clipboard, RightArrow } from '../svg';
 
 type Props = {
   info: Array<Object>,
@@ -10,27 +10,26 @@ type Props = {
   opportunityName: string
 };
 
-const Card = ({ info, title, daysRemain, opportunityName }: Props) => (
+// TODO: Replace data structure when we know how object is structured.
+const ProposalCard = ({ info, title, daysRemain, opportunityName }: Props) => (
   <div className="card">
     <div className="header-section">
       <div>
-        <p id="title">{title}</p>
-        <p id="proposal-name">{opportunityName}</p>
+        <p className="title">{title}</p>
+        <p className="proposal-name">{opportunityName}</p>
       </div>
       <div>
-        <p id="remain-days">{daysRemain}</p>
-        <p id="info-days">Days until Due</p>
+        <p className="remain-days">{daysRemain}</p>
+        <p className="info-days">Days until Due</p>
       </div>
     </div>
     <div className="info-section">
-      {info.map(item => {
-        return (
-          <>
-            <p key={Math.random()}>{item.label}</p>
-            <p key={Math.random()}>{item.info}</p>
-          </>
-        );
-      })}
+      {info.map(item => (
+        <>
+          <p key={uuidv4()}>{item.label}</p>
+          <p key={uuidv4()}>{item.info}</p>
+        </>
+      ))}
     </div>
     <div className="buttons-section">
       <Clipboard className="icon" />
@@ -45,4 +44,4 @@ const Card = ({ info, title, daysRemain, opportunityName }: Props) => (
   </div>
 );
 
-export default Card;
+export default ProposalCard;
