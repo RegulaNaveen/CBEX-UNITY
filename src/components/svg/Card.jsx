0be4be8a080b36e63c90c522a416595248697d96
Carlos Avina <carlos.avina@cbqasolutions.com>
2020-08-12 16:01:43 -0500

    Fix icons

--- src/components/svg/Card.jsx
+++ src/components/svg/Card.jsx
@@ -2,36 +2,34 @@
 import * as React from 'react';
 
 type Props = {
-  className: string
+  className: string,
+  fill?: string
 };
 
-const Card = ({ className }: Props) => {
+const Card = ({ className, fill }: Props) => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      className={className}
-    >
+    <svg width={24} height={24} viewBox="0 0 24 24" className={className}>
       <defs>
         <path
-          id="prefix__a"
+          id="prefix__card__a"
           d="M10 13a1 1 0 011 1v7a1 1 0 01-1 1H3a1 1 0 01-1-1v-7a1 1 0 011-1h7zm11 0a1 1 0 011 1v7a1 1 0 01-1 1h-7a1 1 0 01-1-1v-7a1 1 0 011-1h7zM10 2a1 1 0 011 1v7a1 1 0 01-1 1H3a1 1 0 01-1-1V3a1 1 0 011-1h7zm11 0a1 1 0 011 1v7a1 1 0 01-1 1h-7a1 1 0 01-1-1V3a1 1 0 011-1h7z"
         />
       </defs>
       <g fill="none" fillRule="evenodd">
         <path fill="none" d="M0 0h24v24H0z" />
-        <mask id="prefix__b" fill="#fff">
-          <use xlinkHref="#prefix__a" />
+        <mask id="prefix__card__b" fill="#fff">
+          <use xlinkHref="#prefix__card__a" />
         </mask>
-        <g fill="#000" mask="url(#prefix__b)">
+        <g fill={fill} mask="url(#prefix__card__b)">
           <path d="M0 0h24v24H0z" />
         </g>
       </g>
     </svg>
   );
+};
+
+Card.defaultProps = {
+  fill: '#000'
 };
 
 export default Card;
--- src/components/svg/List.jsx
+++ src/components/svg/List.jsx
@@ -2,10 +2,11 @@
 import * as React from 'react';
 
 type Props = {
-  className: string
+  className: string,
+  fill?: string
 };
 
-const List = ({ className }: Props) => {
+const List = ({ className, fill }: Props) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -17,21 +18,25 @@ const List = ({ className }: Props) => {
     >
       <defs>
         <path
-          id="prefix__a"
+          id="prefix__list__a"
           d="M20.5 18a1.5 1.5 0 010 3h-17a1.5 1.5 0 010-3h17zm0-5a1.5 1.5 0 010 3h-17a1.5 1.5 0 010-3h17zm0-5a1.5 1.5 0 010 3h-17a1.5 1.5 0 010-3h17zm0-5a1.5 1.5 0 010 3h-17a1.5 1.5 0 010-3h17z"
         />
       </defs>
       <g fill="none" fillRule="evenodd">
         <path fill="none" d="M0 0h24v24H0z" />
-        <mask id="prefix__b" fill="#fff">
-          <use xlinkHref="#prefix__a" />
+        <mask id="prefix__list__b" fill="#fff">
+          <use xlinkHref="#prefix__list__a" />
         </mask>
-        <g fill="#000" mask="url(#prefix__b)">
+        <g fill={fill} mask="url(#prefix__list__b)" className={className}>
           <path d="M0 0h24v24H0z" />
         </g>
       </g>
     </svg>
   );
+};
+
+List.defaultProps = {
+  fill: '#000'
 };
 
 export default List;
