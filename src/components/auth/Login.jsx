849cf522d260ddee9ba82c06ec6283319d4bf527
Yael Yañez <yael@cbqasolutions.com>
2020-08-21 01:31:54 -0500

    Login System Redesign

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -3,18 +3,22 @@ import React from 'react';
 import LoginForm from './LoginForm';
 import img from '../../../img/login-background.png';
 
-const Login = () => (
-  <div className="login-wrapper">
-    <div className="gradient-background-img" />
-    <img
-      className="login-background-img"
-      src={img}
-      alt="Login background"
-      role="presentation"
-    />
-    <div className="login-form">
-      <LoginForm />
-      <p className="copyright-text">Copyright @ 2020. All rights reserved</p>
+const Login = () => {
+  return (
+    <div className="login-wrapper">
+      <div className="image-wrapper">
+        <div className="gradient-background-img" />
+        <img
+          className="login-img"
+          src={img}
+          alt="Login background"
+          role="presentation"
+        />
+      </div>
+      <div className="login-form">
+        <LoginForm />
+        <p className="copyright-text">Copyright @ 2020. All rights reserved</p>
+      </div>
     </div>
   </div>
 );
