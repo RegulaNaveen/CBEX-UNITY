13a668c4fdc63c7932c5384585899e7989ac9b51
Fernando Romero Tirado <fernando@cbqasolutions.com>
2020-09-03 12:36:12 -0500

    Removing 100 option from ComplexPagination

--- src/components/common/ComplexPagination.jsx
+++ src/components/common/ComplexPagination.jsx
@@ -63,7 +63,7 @@ class ComplexPagination extends Component<Props, State> {
             <Dropwdown
               value={maxRows.toString()}
               onClick={this.setMaxRows}
-              items={[15, 20, 50, 100]}
+              items={[15, 20, 50]}
             />
           </div>
         </div>
