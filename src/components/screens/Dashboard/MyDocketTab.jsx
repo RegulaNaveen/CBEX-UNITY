2d13e0698ced1ab8030f6234cbed42592baf096a
Fernando Romero Tirado <fernando@cbqasolutions.com>
2020-09-03 13:03:44 -0500

    Dashboard tabs

--- src/components/screens/Dashboard/MyDocketTab.jsx
+++ src/components/screens/Dashboard/MyDocketTab.jsx
@@ -1,56 +1,92 @@
 // @flow
 import React, { Component } from 'react';
-import axios from 'axios';
 import { connect } from 'react-redux';
-import { getProposalTypeView } from '../../../selectors';
+import { chunk } from 'lodash';
+import { getProposalTypeView, getProposals } from '../../../selectors';
+import { getProposalsByStatus } from '../../../actions/proposals-actions';
 import TableView from '../../common/TableView';
 import GridView from '../../common/GridView';
+import ComplexPagination from '../../common/ComplexPagination';
 
 type Props = {
-  selectedViewType: 0 | 1
+  selectedViewType: 0 | 1,
+  proposals: [Object],
+  fetchProposals: Function
 };
 
 type State = {
-  users: [Object]
+  numRows: number,
+  page: number,
+  pageContent: [Object]
 };
 
-class MyDocketTab extends Component<Props, State> {
+class RecentTab extends Component<Props, State> {
   constructor(props: Object) {
     super(props);
-
     this.state = {
-      users: [{}]
+      page: 1,
+      numRows: 15,
+      pageContent: []
     };
   }
 
-  // TODO: remove axios request once redux implementation gets complete
-  async componentDidMount() {
-    const { data } = await axios.get(
-      'https://jsonplaceholder.typicode.com/users'
-    );
+  componentDidMount() {
+    const { fetchProposals } = this.props;
+    fetchProposals('active');
+  }
 
-    this.setState({ users: data });
+  componentDidUpdate(prevProps, prevState) {
+    const { page, numRows } = this.state;
+    const { proposals } = this.props;
+    if (
+      prevState.page !== page ||
+      prevState.numRows !== numRows ||
+      prevProps.proposals !== proposals
+    ) {
+      const pages = chunk(proposals, numRows);
+      this.setPageContent(pages[page - 1]);
+    }
   }
 
   renderSelectedView = () => {
-    const { users } = this.state;
     const { selectedViewType } = this.props;
+    const { pageContent } = this.state;
 
-    if (selectedViewType === 0) return <TableView data={users} />;
-    return <GridView data={users} />;
+    if (selectedViewType === 0) return <TableView data={pageContent} />;
+    return <GridView data={pageContent} />;
   };
 
+  setPage = (page: number) => this.setState({ page });
+
+  setRows = (numRows: number) => this.setState({ numRows });
+
+  setPageContent = (pageContent: Array<Object>) =>
+    this.setState({ pageContent });
+
   render() {
+    const { proposals } = this.props;
     return (
-      <section id="my-docket-tab" className="tab-content">
-        {this.renderSelectedView()}
-      </section>
+      <>
+        <section id="all-tab" className="tab-content">
+          {this.renderSelectedView()}
+        </section>
+        {proposals && (
+          <ComplexPagination
+            totalItems={proposals.length}
+            getCurrentPosition={this.setPage}
+            getMaxRows={this.setRows}
+          />
+        )}
+      </>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  selectedViewType: getProposalTypeView(state)
+  selectedViewType: getProposalTypeView(state),
+  proposals: getProposals(state)
 });
 
-export default connect(mapStateToProps)(MyDocketTab);
+const mapDispatchToProps = { fetchProposals: getProposalsByStatus };
+
+export default connect(mapStateToProps, mapDispatchToProps)(RecentTab);
--- src/components/screens/Dashboard/index.jsx
+++ src/components/screens/Dashboard/index.jsx
@@ -3,28 +3,26 @@ import React from 'react';
 import Toolbar from '../../Toolbar';
 import Tabbar from '../../common/Tabbar';
 import MyDocketTab from './MyDocketTab';
-import RecentTab from './RecetTab';
+import RecentTab from './RecentTab';
 import AllTab from './AllTab';
 
-const Dashboard = () => {
-  return (
-    <div id="dashboard">
-      <Toolbar selected="dashboard" />
-      <div className="tab-wrapper">
-        <Tabbar>
-          <div label="My Docket">
-            <MyDocketTab />
-          </div>
-          <div label="Recent">
-            <RecentTab />
-          </div>
-          <div label="All">
-            <AllTab />
-          </div>
-        </Tabbar>
-      </div>
+const Dashboard = () => (
+  <div id="dashboard">
+    <Toolbar selected="dashboard" />
+    <div className="tab-wrapper">
+      <Tabbar>
+        <div label="My Docket">
+          <MyDocketTab />
+        </div>
+        <div label="Recent">
+          <RecentTab />
+        </div>
+        <div label="All">
+          <AllTab />
+        </div>
+      </Tabbar>
     </div>
-  );
-};
+  </div>
+);
 
 export default Dashboard;
