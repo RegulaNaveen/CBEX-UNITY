27e1fd96b4c7419b71b6ffb9065e95ea462cdcdc
Fernando Romero Tirado <fernando@cbqasolutions.com>
2020-09-03 12:08:29 -0500

    Proposals reducers

--- src/reducers/proposals.js
+++ src/reducers/proposals.js
@@ -3,11 +3,27 @@ import { Map, fromJS } from 'immutable';
 import { REDUX_TYPES } from '../constants';
 import type { ApiAction } from '../actions/action-types';
 
-const { SET_PROPOSAL_VIEW_TYPE } = REDUX_TYPES.PROPOSALS;
+const {
+  SET_PROPOSAL_VIEW_TYPE,
+  ON_GET_PROPOSALS,
+  ERROR_ON_GET_PROPOSALS
+} = REDUX_TYPES.PROPOSALS;
 
 const INITIAL_STATE: Map = fromJS({
-  selectedViewType: 0
+  selectedViewType: 0,
+  proposals: undefined,
+  proposalsError: undefined
 });
+
+const setProposals = (state: Map, action: Object): Map => {
+  const { proposals } = action.payload;
+  return state.set('proposals', proposals);
+};
+
+const setProposalsError = (state: Map, action: Object): Map => {
+  const { error } = action.payload;
+  return state.set('proposalsError', error);
+};
 
 const setProposalViewType = (state: Map, action: Object): Map => {
   const { payload } = action;
@@ -15,7 +31,9 @@ const setProposalViewType = (state: Map, action: Object): Map => {
 };
 
 const actionMap = {
-  [SET_PROPOSAL_VIEW_TYPE]: setProposalViewType
+  [SET_PROPOSAL_VIEW_TYPE]: setProposalViewType,
+  [ON_GET_PROPOSALS]: setProposals,
+  [ERROR_ON_GET_PROPOSALS]: setProposalsError
 };
 
 export default function(
