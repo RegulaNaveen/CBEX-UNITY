da2ed29590140009bad9dd431fe73ec38f5d162f
Fernando Romero Tirado <fernando@cbqasolutions.com>
2020-09-03 12:50:00 -0500

    Date difference function

--- src/utils/DateUtils.js
+++ src/utils/DateUtils.js
@@ -10,3 +10,12 @@ export const parseDate = (date: string, format: string) => {
 
 export const formatDate = (date: Date, format: string) =>
   dateFnsFormat(date, format, {});
+
+export const dateDiffInDays = (date: Object): number => {
+  const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+  const today = new Date();
+  const utc1 = Date.UTC(today.getFullYear(), today.getMonth(), today.getDate());
+  const utc2 = Date.UTC(date.getFullYear(), date.getMonth(), date.getDate());
+
+  return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+};
