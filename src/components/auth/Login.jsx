a0e86e2eb87d710cb9a3f92591fe3c569ccf60c8
Fernando Romero Tirado <fernando@cbqasolutions.com>
2020-09-11 15:55:21 -0500

    Removing Lookup example.

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -2,28 +2,21 @@
 import React from 'react';
 import LoginForm from './LoginForm';
 import img from '../../../img/login-background.png';
-import Lookup from '../common/Lookup';
 
-const Login = () => {
-  return (
-    <div className="login-wrapper">
-      <div className="gradient-background-img" />
-      <img
-        className="login-background-img"
-        src={img}
-        alt="Login background"
-        role="presentation"
-      />
-      <div className="login-form">
-        <LoginForm />
-        <Lookup
-          data={[{ name: 'Yael' }, { name: 'Yaelasd' }, { name: 'Alan' }]}
-          defaultValue="Yael"
-          getSelectedItem={val => console.log(val)}
-        />
-        <p className="copyright-text">Copyright @ 2020. All rights reserved</p>
-      </div>
+const Login = () => (
+  <div className="login-wrapper">
+    <div className="gradient-background-img" />
+    <img
+      className="login-background-img"
+      src={img}
+      alt="Login background"
+      role="presentation"
+    />
+    <div className="login-form">
+      <LoginForm />
+      <p className="copyright-text">Copyright @ 2020. All rights reserved</p>
     </div>
-  );
-};
+  </div>
+);
+
 export default Login;
