bb8daa3c9c1683d28f75cdbe940c3ce81e16c27f
Fernando Romero Tirado <fernando@cbqasolutions.com>
2020-09-17 17:48:40 -0500

    WIP: Non text dashboard filters. (Values for dropdowns pending)

--- src/components/common/DashboardFilters.jsx
+++ src/components/common/DashboardFilters.jsx
@@ -1,87 +1,120 @@
 // @flow
-import React from 'react';
+import React, { useCallback } from 'react';
 import InputField from './InputField';
-import Dropdown from './Dropdown';
-import DatePicker from './DatePicker';
+import FilterDropDown from './FilterDropDown';
+import DateRange from './DateRange';
+import UserLookup from './UserLookup';
 
 type Props = {
-  onChange: Function
+  onTextFilterChange: Function,
+  onDropDownFilterChange: Function,
+  onDateRangeChange: Function
 };
 
-const DashboardFilters = ({ onChange }: Props) => (
-  <div id="dashboard-filters">
-    <div className="filter-wrapper">
-      <InputField
-        title="Opportunity #"
-        id="opportunity #"
-        onChange={onChange}
-        placeholder="Opportunity number"
-        type="text"
-      />
+const DashboardFilters = ({
+  onTextFilterChange,
+  onDropDownFilterChange,
+  onDateRangeChange
+}: Props) => {
+  const changeDate = useCallback(range => {
+    onDateRangeChange('bid due date', range);
+  });
+
+  const changeUser = useCallback(value => {
+    onDropDownFilterChange('teamMember', value);
+  });
+
+  return (
+    <div id="dashboard-filters">
+      <div className="filter-wrapper">
+        <InputField
+          title="Opportunity #"
+          id="opportunity #"
+          onChange={onTextFilterChange}
+          placeholder="Opportunity number"
+          type="text"
+        />
+      </div>
+      <div className="filter-wrapper">
+        <InputField
+          title="Opportunity name"
+          id="opportunityName"
+          onChange={onTextFilterChange}
+          placeholder="Opportunity name"
+          type="text"
+        />
+      </div>
+      <div className="filter-wrapper">
+        <InputField
+          title="Customer"
+          id="account"
+          onChange={onTextFilterChange}
+          placeholder="Customer"
+          type="text"
+        />
+      </div>
+      <div className="filter-wrapper">
+        <InputField
+          title="Protocol #"
+          id="protocol #"
+          onChange={onTextFilterChange}
+          placeholder="Protocol number"
+          type="text"
+        />
+      </div>
+      <div className="filter-wrapper">
+        <FilterDropDown
+          title="Phase"
+          id="phase"
+          onChange={onDropDownFilterChange}
+          items={['1', '2', '3']}
+        />
+      </div>
+      <div className="filter-wrapper">
+        <InputField
+          title="Product"
+          id="product"
+          onChange={onTextFilterChange}
+          placeholder="Product"
+          type="text"
+        />
+      </div>
+      <div className="filter-wrapper">
+        <FilterDropDown
+          title="Therapeutic area"
+          id="therapeuticArea"
+          onChange={onDropDownFilterChange}
+          items={['1', '2', '3']}
+        />
+      </div>
+      <div className="filter-wrapper">
+        <FilterDropDown
+          title="Indication"
+          id="indication"
+          onChange={onDropDownFilterChange}
+          items={['1', '2', '3']}
+        />
+      </div>
+      <div className="filter-wrapper">
+        <DateRange
+          label="Bid due date"
+          id="bid due date"
+          onSetRange={changeDate}
+        />
+      </div>
+      <div className="filter-wrapper">
+        <FilterDropDown
+          title="Opportunity status"
+          id="opportunityStatus"
+          onChange={onDropDownFilterChange}
+          items={['a', 'b', 'c']}
+        />
+      </div>
+      <div className="filter-wrapper">
+        <UserLookup title="Team member" id="teamMember" onChange={changeUser} />
+      </div>
     </div>
-    <div className="filter-wrapper">
-      <InputField
-        title="Opportunity name"
-        id="opportunityName"
-        onChange={onChange}
-        placeholder="Opportunity name"
-        type="text"
-      />
-    </div>
-    <div className="filter-wrapper">
-      <InputField
-        title="Customer"
-        id="account"
-        onChange={onChange}
-        placeholder="Customer"
-        type="text"
-      />
-    </div>
-    <div className="filter-wrapper">
-      <InputField
-        title="Protocol #"
-        id="protocol #"
-        onChange={onChange}
-        placeholder="Protocol number"
-        type="text"
-      />
-    </div>
-    <div className="filter-wrapper">
-      <Dropdown title="Phase" id="phase" onClick={onChange} />
-    </div>
-    <div className="filter-wrapper">
-      <InputField
-        title="Product"
-        id="product"
-        onChange={onChange}
-        placeholder="Product"
-        type="text"
-      />
-    </div>
-    <div className="filter-wrapper">
-      <Dropdown
-        title="Therapeutic area"
-        id="therapeuticArea"
-        onClick={onChange}
-      />
-    </div>
-    <div className="filter-wrapper">
-      <Dropdown title="Indication" id="indication" onClick={onChange} />
-    </div>
-    <div className="filter-wrapper">
-      <DatePicker label="Bid due date" id="bid due date" onChange={onChange} />
-    </div>
-    <div className="filter-wrapper">
-      <Dropdown
-        title="Opportunity status"
-        id="opportunityStatus"
-        onClick={onChange}
-      />
-    </div>
-    <div className="filter-wrapper">
-      <InputField title="Team member" id="teamMember" onChange={onChange} />
-    </div>
-  </div>
-);
+  );
+};
 
 export default DashboardFilters;
