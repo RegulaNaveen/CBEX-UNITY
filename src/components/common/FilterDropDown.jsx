bafe792a35cdc99f7f13ee708b85ddb416cc7c2b
Fernando Romero Tirado <fernando@cbqasolutions.com>
2020-09-17 13:11:36 -0500

    FilterDropDown  component.

---
+++ src/components/common/FilterDropDown.jsx
@@ -0,0 +1,14 @@
+// @flow
+import React, { useCallback } from 'react';
+import Dropdown from './Dropdown';
+
+type Props = { id: string, onChange: Function, title: string, items: Array };
+
+const FilterDropDown = ({ id, onChange, title, items }: Props) => {
+  const filterChange = useCallback(value => onChange(id, value));
+  return (
+    <Dropdown title={title} id={id} onClick={filterChange} items={items} />
+  );
+};
+
+export default FilterDropDown;
