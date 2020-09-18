fb388c588d1fa9544b3a7eaf0bca8a5ad3f25cfb
Yael Yañez <yael@cbqasolutions.com>
2020-09-18 12:53:14 -0500

    Iframe implementation

--- src/components/screens/Proposal/Documents.jsx
+++ src/components/screens/Proposal/Documents.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import Iframe from 'react-iframe';
 
-const Documents = () => <h3>Documents</h3>;
+const Documents = () => {
+  return (
+    <div className="documents">
+      <Iframe url="https://account.box.com/login" width="100%" height="100%" />
+    </div>
+  );
+};
 
 export default Documents;
