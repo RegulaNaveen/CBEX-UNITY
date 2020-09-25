e832a1438d10ee03e43dd6d7214781ca97f9ba70
Fernando Romero Tirado <fernando@cbqasolutions.com>
2020-09-25 10:45:31 -0500

    Showing the correct numbers for pagination and not showing with empty data.

--- src/components/screens/Dashboard/MyDocketTab.jsx
+++ src/components/screens/Dashboard/MyDocketTab.jsx
@@ -81,7 +81,17 @@ class RecentTab extends Component<Props, State> {
     this.setState({ pageContent });
 
   render() {
-    const { proposals, loading } = this.props;
+    const {
+      proposals,
+      loading,
+      isFilteringProposals,
+      filteredProposals
+    } = this.props;
+
+    const showPagination = isFilteringProposals
+      ? !isEmpty(filteredProposals)
+      : !isEmpty(proposals);
+
     return loading ? (
       <Loader
         type="TailSpin"
@@ -95,9 +105,11 @@ class RecentTab extends Component<Props, State> {
         <section id="all-tab" className="tab-content">
           {this.renderSelectedView()}
         </section>
-        {!isEmpty(proposals) && (
+        {showPagination && (
           <ComplexPagination
-            totalItems={proposals.length}
+            totalItems={
+              isFilteringProposals ? filteredProposals.length : proposals.length
+            }
             getCurrentPosition={this.setPage}
             getMaxRows={this.setRows}
           />
