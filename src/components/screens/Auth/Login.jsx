efaed7dad14522b759762321fd3a765992e1b5e3
Yael Yañez <yael@cbqasolutions.com>
2020-10-09 10:30:05 -0500

    Disable loader on login loading

--- src/components/screens/Auth/Login.jsx
+++ src/components/screens/Auth/Login.jsx
@@ -1,7 +1,5 @@
 // @flow
 import React from 'react';
-import Loader from 'react-loader-spinner';
-import classNames from 'classnames';
 import { Microsoft } from '../../svg';
 import img from '../../../../img/login-background.png';
 import { API } from '../../../constants';
@@ -14,8 +12,6 @@ const Login = () => {
       `${COGNITO_HOST}/oauth2/authorize?identity_provider=AzureAD&redirect_uri=${REDIRECTION_URL}&response_type=CODE&client_id=${CLIENT_ID}&scope=aws.cognito.signin.user.admin email openid phone profile`
     );
   }
-
-  const getIsAuthInProgress = !!localStorage.getItem('isAuthInProgress');
 
   return (
     <div className="sso-login-wrapper">
@@ -33,20 +29,13 @@ const Login = () => {
           <p className="form-title">IQVIA Unity</p>
           <button
             type="button"
-            className={classNames('azure-login-button', {
-              'is-loading': getIsAuthInProgress
-            })}
-            disabled={getIsAuthInProgress}
+            className="azure-login-button"
             onClick={handleAmplifyLogin}
           >
-            {getIsAuthInProgress ? (
-              <Loader type="TailSpin" color="#FFFFFF" height={20} width={20} />
-            ) : (
-              <>
-                <Microsoft width={20} height={20} />
-                Log in with SSO
-              </>
-            )}
+            <>
+              <Microsoft width={20} height={20} />
+              Log in with SSO
+            </>
           </button>
         </div>
         <p className="copyright-text">Copyright @ 2020. All rights reserved</p>
