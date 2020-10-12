8c7c5d60735e9bc2d0b3bbf6cd1b332742fa37b6
Yael Yañez <yael@cbqasolutions.com>
2020-10-12 10:16:45 -0500

    Add local uat env & fix table and grid view for proposals

--- src/selectors/proposals.js
+++ src/selectors/proposals.js
@@ -1,8 +1,14 @@
 // @flow
 import { Map } from 'immutable';
+import { isEmpty } from 'lodash';
 
-export const getProposals = (proposals: Map): Array<Object> =>
-  proposals.get('proposals');
+export const getProposals = (proposals: Map): Array<Object> => {
+  const notEmptyProposals = !isEmpty(proposals.get('proposals'))
+    ? proposals.get('proposals').filter(proposal => !isEmpty(proposal))
+    : [];
+
+  return notEmptyProposals;
+};
 
 export const getProposalsLoading = (proposals: Map): boolean =>
   proposals.get('proposalsLoading');
@@ -10,8 +16,13 @@ export const getProposalsLoading = (proposals: Map): boolean =>
 export const getProposalTypeView = (proposals: Map): 0 | 1 =>
   proposals.get('selectedViewType');
 
-export const getFilteredProposals = (proposals: Map): Map =>
-  proposals.get('filteredProposals');
+export const getFilteredProposals = (proposals: Map): Map => {
+  const notEmptyProposals = !isEmpty(proposals.get('filteredProposals'))
+    ? proposals.get('filteredProposals').filter(proposal => !isEmpty(proposal))
+    : [];
+
+  return notEmptyProposals;
+};
 
 export const getIsFilteringProposals = (proposals: Map): boolean =>
   proposals.get('isFiltering');
