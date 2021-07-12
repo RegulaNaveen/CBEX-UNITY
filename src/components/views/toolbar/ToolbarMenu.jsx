6917b600641bc6aad67bd77912204e283523daf7
TIWARI <rahul.tiwari@iqvia.com>
2021-07-12 12:30:13 +0530

    Ordering of Roles on Unity

--- src/components/views/toolbar/ToolbarMenu.jsx
+++ src/components/views/toolbar/ToolbarMenu.jsx
@@ -72,7 +72,7 @@ export class ToolbarMenuComponent extends PureComponent<Props, State> {
     const { rolesList, isRolesLoading } = this.props;
     const name = getUserName();
     const email = getUserEmail();
-
+    
     return (
       <div className="toolbar-account-menu">
         <p className="toolbar-account-menu-name">{name}</p>
@@ -88,7 +88,7 @@ export class ToolbarMenuComponent extends PureComponent<Props, State> {
               id="dd-team-member"
               title="User Role"
               placeholder="Select"
-              items={rolesList}
+              items={rolesList ? rolesList.sort() : []}
               onClick={this.onRoleChange}
               value={roleName}
             />
