a07c56e32cbd87e25c7c9591be4fb62841aed2f5
TIWARI <rahul.tiwari@iqvia.com>
2021-07-20 16:59:33 +0530

    fix changes

--- src/components/views/Notepad/index.jsx
+++ src/components/views/Notepad/index.jsx
@@ -164,7 +164,7 @@ Notepad.propTypes = {
   change: PropTypes.func.isRequired,
   fetchingNotes: PropTypes.bool.isRequired,
   addingNote: PropTypes.bool.isRequired,
-  mode: PropTypes.oneOf(REDUX_TYPES.NOTEPAD).isRequired,
+  mode: PropTypes.string.isRequired,
   selectedtitle: PropTypes.string.isRequired
 };
 
