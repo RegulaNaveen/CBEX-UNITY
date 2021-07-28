10fe05ee74768f3be392022bb6030861328fd2b8
chinna <chinnachamy.soundarajan@iqvia.com>
2021-07-28 16:40:08 +0530

    added pipeline [ci skip]

---
+++ devops/scripts/set_env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ $CI_COMMIT_REF_NAME == "master" ]
+then
+   echo "Environment=Prod" > build.env
+elif [ $CI_COMMIT_REF_NAME == "uat" ]
+then
+   echo "Environment=uat" > build.env
+elif [ $CI_COMMIT_REF_NAME == "qa" ]
+then
+   echo "Environment=qa" > build.env
+elif [ $CI_COMMIT_REF_NAME == "sprintdev" ]
+then
+   echo "Environment=sdev" > build.env
+elif [ $CI_COMMIT_REF_NAME == "dev" ]
+then
+   echo "Environment=dev" > build.env
+elif [[ $branch == "feature/"* ]]
+then
+   echo "Environment=dev" > build.env  
+else
+   exit
+fi
