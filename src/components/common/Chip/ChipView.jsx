21551774a38b0d44faa8e312a414bca4a9b51d34
TIWARI <rahul.tiwari@iqvia.com>
2021-09-24 17:32:39 +0530

    tooltip added

--- src/components/common/Chip/ChipView.jsx
+++ src/components/common/Chip/ChipView.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import Tag from 'apollo-react/components/Tag';
 import PropTypes from 'prop-types';
 import Check from 'apollo-react-icons/Check';
+import Tooltip from 'apollo-react/components/Tooltip';
 
 const ChipView = ({ label, answer }) => {
   return (
     <div>
-      <Tag
-        Icon={answer ? Check : ''}
-        label={label}
-        variant={answer ? 'green' : 'blue'}
-      />
+      <Tooltip title={ label } placement="top">
+        <Tag
+          Icon={ answer ? Check : '' }
+          label={ label }
+          variant={ answer ? 'green' : 'blue' }
+        />
+      </Tooltip>
     </div>
   );
 };
