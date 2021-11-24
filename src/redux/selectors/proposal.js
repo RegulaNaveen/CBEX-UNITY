ddd89c9489282ef611ea49b78bdd23800a583ce1
TIWARI <rahul.tiwari@iqvia.com>
2021-11-24 18:51:47 +0530

    latest

--- src/redux/selectors/proposal.js
+++ src/redux/selectors/proposal.js
@@ -72,8 +72,8 @@ const getQuestionSections = (items: Array<Object>) => {
 
 function getRecentAnswer(answers) {
   const recentAnswer = last(answers);
-  if (recentAnswer && recentAnswer.answer && String(recentAnswer.answer).trimEnd().trimStart().length) {
-    return String(recentAnswer.answer).trimEnd().trimStart().length > 0
+  if (recentAnswer && recentAnswer.answer && String(recentAnswer.answer).trim().length) {
+    return String(recentAnswer.answer).trim().length > 0
       ? recentAnswer.answer
       : 'Not defined yet.';
   }
