0e82b0ff2ecb0bfeaf5102366ac93381484da767
TIWARI <rahul.tiwari@iqvia.com>
2021-11-25 13:17:47 +0530

    fixed text input issue

--- src/components/common/Question.jsx
+++ src/components/common/Question.jsx
@@ -73,7 +73,7 @@ export class TaskRow extends Component<Props, State> {
     if (!isEmpty(textValue.replace(/\r?\n|\r| /g, ''))) {
       if (lastAnswer !== textValue)
         setProposalAnswer(proposalId, questionId, textValue, userData);
-    } else if (!textValue && lastAnswer.trim()) {
+    } else if (!textValue.trim() && lastAnswer.trim()) {
       setProposalAnswer(proposalId, questionId, ' ', userData);
     }
 
@@ -219,6 +219,7 @@ export class TaskRow extends Component<Props, State> {
 
     switch (type) {
       case 'text':
+        answerValue = !(String(answerValue).trim()) ? '' : String(answerValue).trim();
         return (
           <TextField
             className="proposal-text-area"
