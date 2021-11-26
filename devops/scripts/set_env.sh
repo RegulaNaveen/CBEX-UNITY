b96f6b020ec3b9b00a511d07e11720c337d1d8b4
PANDEY <sandeep.pandey@quintiles.com>
2021-11-26 14:16:14 +0530

    sonar jobs

--- devops/scripts/set_env.sh
+++ devops/scripts/set_env.sh
@@ -20,6 +20,9 @@ then
 elif [[ $CI_COMMIT_REF_NAME == "bugfix/"* ]]
 then
    echo "Environment=dev" > build.env 
+elif [[ $CI_COMMIT_REF_NAME == "sonarcicd"* ]]
+then
+   echo "Environment=dev" > build.env 
 else
    exit
 fi
