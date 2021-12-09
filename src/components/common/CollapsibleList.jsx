6ee55e51019503b51db211d1f706c0d1ac49bdcc
Keerthiprasath <keerthiprasath.chandran@iqvia.com>
2021-12-09 20:09:05 +0530

    IQVIACVEXU-754 added tooltip to question text

--- src/components/common/CollapsibleList.jsx
+++ src/components/common/CollapsibleList.jsx
@@ -239,6 +239,7 @@ class CollapsibleList extends Component<Props, State> {
                     sfField={questionConfig.get('sfField')}
                     sectionName={title}
                     setQuestionToDisplayHistory={setQuestionToDisplayHistory}
+                    questionHint={questionConfig.get('questionHint', '')}
                   />
                 )
               );
