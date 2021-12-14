cc930c33199a0fd74bc45a9ee0871dae14b9b371
u1110796 <nihal.prathep@iqvia.com>
2021-12-14 15:34:43 +0530

    button disable feature

--- src/components/common/atoms/Buttons.jsx
+++ src/components/common/atoms/Buttons.jsx
@@ -6,20 +6,29 @@ type Props = {
   id?: string,
   children: any,
   onClick: Function,
-  className?: string
+  className?: string,
+  disabled?: boolean
 };
 
 const defaultProps = {
   id: undefined,
-  className: undefined
+  className: undefined,
+  disabled: false
 };
 
-export const PrimaryButton = ({ id, onClick, children, className }: Props) => (
+export const PrimaryButton = ({
+  id,
+  onClick,
+  children,
+  className,
+  disabled
+}: Props) => (
   <button
     id={id}
     type="button"
     className={classnames('primary-button', className)}
     onClick={onClick}
+    disabled={disabled}
   >
     {children}
   </button>
