9dbc9b65daa37dc7dde98e7d87fa4309328ff9de
Shahrukh <shahrukh.khan2@iqvia.com>
2021-12-15 16:40:31 +0530

    Update AutoComplete.jsx

--- src/components/common/atoms/inputs/AutoComplete.jsx
+++ src/components/common/atoms/inputs/AutoComplete.jsx
@@ -1,71 +1,67 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import AutocompleteV2 from 'apollo-react/components/AutocompleteV2';
 import { connect } from 'react-redux';
 import { getLookupUsers } from '../../../../redux/selectors';
 
-function extractEmails(str) {
-  const result = String(str).match(
-    /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi
-  );
-  return result && result.length ? result[0] : '';
+function extractEmails (str){
+    let result =  String(str).match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi);
+    return result && result.length ? result[0] : '' 
 }
 
-const Autocomplete = props => {
-  const [value, setValue] = useState([]);
-  const text = String(props?.text)
-    .trimStart()
-    .trimEnd();
+const Autocomplete = (props) => {
+    const [value, setValue] = useState([]);
+    const text = String(props?.text).trimStart().trimEnd();
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-    const proposaluser = newValue.map(v => {
-      return v.email
-        ? `${v.label}(${v.email})`
-        : `${v.label}(${extractEmails(v.label)})`;
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+        const proposaluser = newValue.map(v=>{
+            return v.email ? v.label+"("+v.email+")" : v.label+"("+extractEmails(v.label)+")"
+        })
+        if(proposaluser.length == 0)
+            props.onChange(" ",text);
+        else
+            props.onChange(proposaluser.join(","),text);
+    };
+    const UserNameByEmail = {};
+    const proposalusers = props?.users?.map(v=>{
+        UserNameByEmail[v.email] = v.name.split(",").join(" ");
+        return { label: v.name.split(",").join(" "), email: v.email}
     });
-    if (proposaluser.length == 0) props.onChange(' ', text);
-    else props.onChange(proposaluser.join(','), text);
-  };
-  const UserNameByEmail = {};
-  const proposalusers = props?.users?.map(v => {
-    UserNameByEmail[v.email] = v.name;
-    return { label: v.name, email: v.email };
-  });
-  useEffect(() => {
-    if (text.length) {
-      const Val = text.split(',').map(v => {
-        const email = extractEmails(v);
-        return { label: UserNameByEmail[email] || email, email };
-      });
-      setValue(Val);
-    }
-  }, [text]);
+    useEffect(() => {
+        if(Boolean(text.length)){
+            let Val = text.split(",").map(v=>{
+                let email = extractEmails(v) || v;
+                return { label: UserNameByEmail[email] || email, email: email }
+            });
+            setValue(Val);
+        }
+    }, [text]);
 
-  return (
-    <div style={{ maxWidth: 500 }}>
-      <AutocompleteV2
-        fullWidth
-        multiple
-        onFocus={e => {
-          props.onFocus();
-        }}
-        onBlur={e => {
-          props.onBlur();
-        }}
-        source={proposalusers || []}
-        value={value}
-        chipColor="white"
-        size="small"
-        limitChips={5}
-        matchFrom="any"
-        onChange={handleChange}
-        noOptionsText="No matches found"
-      />
-    </div>
-  );
-};
+    return (
+        <div style={{ maxWidth: 500 }}>
+            <AutocompleteV2
+                fullWidth
+                multiple
+                source={proposalusers || []}
+                value={value}
+                chipColor="white"
+                size="small"
+                limitChips={5}
+                matchFrom="any"
+                onChange={handleChange}
+                noOptionsText="No matches found"
+                onFocus={e => {
+                    props.onFocus();
+                }}
+                onBlur={e => {
+                    props.onBlur();
+                }}
+            />
+        </div>
+    )
+}
 const mapStateToProps = state => ({
-  users: getLookupUsers(state)
-});
-
+    users: getLookupUsers(state)
+  });
+  
 export default connect(mapStateToProps)(Autocomplete);
