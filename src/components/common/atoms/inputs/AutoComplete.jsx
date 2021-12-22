ab713bdc54f3acda61ce4e62d5a1cb5b1eaed7c3
Sultan <ashiq.sultan@iqvia.com>
2021-12-22 18:47:47 +0530

    extractName function added to get name for Salesforce answers

--- src/components/common/atoms/inputs/AutoComplete.jsx
+++ src/components/common/atoms/inputs/AutoComplete.jsx
@@ -5,7 +5,19 @@ import { getLookupUsers } from '../../../../redux/selectors';
 
 function extractEmails (str){
     let result =  String(str).match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi);
-    return result.length ? result[0] : '' 
+    return result ? (result.length ? result[0] : '') : '';
+}
+
+/**
+ * Extracts name from the string format: `Firstname Lastname(name@example.com)`
+ */
+function extractName(str) {
+    const splirt_array = str.split('(');
+    return splirt_array // check null
+      ? splirt_array.length > 0
+        ? splirt_array[0].trim()
+        : ''
+      : '';
 }
 
 const Autocomplete = (props) => {
@@ -30,8 +42,9 @@ const Autocomplete = (props) => {
     useEffect(() => {
         if(Boolean(text.length)){
             let Val = text.split(",").map(v=>{
-                let email = extractEmails(v);
-                return { label: UserNameByEmail[email] || email, email: email }
+                let email = extractEmails(v)|| v;
+                let label = UserNameByEmail[email] || extractName(v) || email; 
+                return { label, email}
             });
             setValue(Val);
         }
