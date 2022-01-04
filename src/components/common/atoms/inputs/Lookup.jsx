745ceb7ae3d49c6689a3929ebda409b2b45f1344
u1110796 <nihal.prathep@iqvia.com>
2022-01-04 18:53:49 +0530

    clear filterData on clear all filter click

--- src/components/common/atoms/inputs/Lookup.jsx
+++ src/components/common/atoms/inputs/Lookup.jsx
@@ -52,7 +52,7 @@ class Lookup extends Component<Props, State> {
   componentDidMount() {
     document.addEventListener('cleantemmmeberinput', e => {
       if (e && e.detail) {
-        this.setState({ searchValue: '' });
+        this.setState({ searchValue: '', filteredData: [] });
       }
     });
   }
