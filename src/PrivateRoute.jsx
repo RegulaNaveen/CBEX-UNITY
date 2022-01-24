b63584f1fa19569dd4796d21cd0aaf08b277e034
TIWARI <rahul.tiwari@iqvia.com>
2022-01-24 16:08:08 +0530

    latest

--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -6,40 +6,28 @@ import type { Match } from 'react-router-dom';
 
 import { LOGIN } from './routes';
 import { saveRedirectURL } from './utils/StorageUtils';
-import { getProposalInfo } from './api/proposal';
-import { withRouter } from "react-router";
 
 type Props = { component: ElementType, match?: Match };
 
-const PrivateRoute =  ({ component: Component, ...rest }: Props) => {
-  // if(rest && String(rest.path).includes("/proposals/:id?")){
-  //   const { computedMatch } = rest;
-  //   if(computedMatch && computedMatch.params && computedMatch.params.id){
-  //     return getProposalInfo(computedMatch.params.id).then(res => {
-  //       let url = '/opportunity/'+ res.proposal.proposalDetails['CRM #'];
-  //       window.location.replace(url);
-  //     })
-  //   }
-  // }else{
-    function renderRoute(props: any) {
-      const {
-        history: {
-          location: { pathname, search }
-        }
-      } = props;
-      const isAuthenticated = !!localStorage.getItem('access_token');
-      if (!isAuthenticated) {
-        saveRedirectURL(pathname + search);
+const PrivateRoute = ({ component: Component, ...rest }: Props) => {
+  function renderRoute(props: any) {
+    const {
+      history: {
+        location: { pathname, search }
       }
-      return isAuthenticated ? <Component {...props} /> : <Redirect to={LOGIN} />;
+    } = props;
+    const isAuthenticated = !!localStorage.getItem('access_token');
+    if (!isAuthenticated) {
+      saveRedirectURL(pathname + search);
     }
-  
-    return <Route {...rest} render={renderRoute} />;
+    return isAuthenticated ? <Component {...props} /> : <Redirect to={LOGIN} />;
   }
-// };
+
+  return <Route {...rest} render={renderRoute} />;
+};
 
 PrivateRoute.defaultProps = {
   match: undefined
 };
 
-export default withRouter(PrivateRoute);
+export default PrivateRoute;
