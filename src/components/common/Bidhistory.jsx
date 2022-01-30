d250866980d74e11656cd79476dd5af69a7cc18c
u1110796 <nihal.prathep@iqvia.com>
2022-01-31 00:43:23 +0530

    bid history component style and summary update

--- src/components/common/Bidhistory.jsx
+++ src/components/common/Bidhistory.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, connect } from 'react-redux';
 import chevronRight from '../../../img/chevron-right.svg';
 import chevronDown from '../../../img/chevron-down.svg';
 import { getBidList, getSelectedBid } from '../../redux/selectors/proposal';
 import { parseMomentDate } from '../../utils/DateUtils';
+import { Checkmark } from '../svg';
+import { changeBid } from '../../redux/actions/proposal-actions';
 
-const BidHistory = () => {
+const BidHistory = ({ changeBid }) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
 
   const bidList = useSelector(getBidList);
@@ -54,16 +56,20 @@ const BidHistory = () => {
           <div className="bid-history-content-wrapper">
             <div className="bid-history-left-content">
               <div
-                className="task-table-headers"
+                className="task-table-headers bid-history-title"
                 role="button"
                 onClick={handleCollapse}
                 onKeyPress={handleKeyPress}
                 tabIndex={-1}
               >
-                <div className="task-title">
+                <div className="task-title bid-title">
                   <p>Bid History</p>
                 </div>
               </div>
+              <p className="helper-text">
+                Select any bid to review questions and answers from that
+                iteration
+              </p>
               <div className="task-table-row bid-list-wrapper">
                 <div className="bid-list-header-row">
                   <div>Bid Number</div>
@@ -72,24 +78,46 @@ const BidHistory = () => {
                 {bidList.length > 0 &&
                   bidList.map(item => (
                     <div
+                      onClick={() => changeBid(item)}
                       className={`bid-list-row ${
-                        selectedBid === item.bidId ? 'selected-bid' : ''
+                        selectedBid.get('id') === item.bidId
+                          ? 'selected-bid'
+                          : ''
                       }`}
+                      key={item.bidId}
                     >
                       <div>
-                        {item.bidName}{' '}
-                        {item.bidId === selectedBid && '(Current)'}
+                        {item.bidName} {item.isCurrent && '(Current)'}
                       </div>
                       <div>{parseMomentDate(item.bidDate)}</div>
+                      {item.bidId === selectedBid.get('id') && (
+                        <Checkmark
+                          className="selected-bid-check"
+                          style={{ marginLeft: '6px' }}
+                        />
+                      )}
                     </div>
                   ))}
               </div>
             </div>
-            <div className="bid-history-right-content">hello</div>
+            <div className="bid-history-right-content">
+              <p className="review-title">For Review</p>
+              <p className="summary-title">Bid Change Summary</p>
+              <p className="pertinent-details-title">
+                Pertinent Details / Specific Rebid Request
+              </p>
+              <div className="pertinent-details-section">
+                <p>Customer Requested we evaluate the site/country</p>
+              </div>
+              <p className="helper-text">
+                This text was provided by Salesforce user when latest bid was
+                created
+              </p>
+            </div>
           </div>
         </div>
       )}
     </div>
   );
 };
-export default BidHistory;
+export default connect(null, { changeBid })(BidHistory);
