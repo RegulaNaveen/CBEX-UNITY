67eb7ea0f77d2dd3128840211d1414ad84aa7d35
TIWARI <rahul.tiwari@iqvia.com>
2022-03-14 16:14:45 +0530

    Clicking on unity link in intake redirecting to documents page

--- src/components/screens/Opportunity/index.jsx
+++ src/components/screens/Opportunity/index.jsx
@@ -69,8 +69,14 @@ export class Opportunity extends Component<Props, State> {
       expandAllSections,
       trackPageView,
       eventCategories,
+      location: { search },
       match: { params }
     } = this.props;
+    let sfflag = new URLSearchParams(search).get('sf');
+    if(sfflag){
+      localStorage.removeItem('proposalTypeView')
+    }
+
     expandAllSections(false);
     const selectedView = localStorage.getItem('proposalTypeView');
 
