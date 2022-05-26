6ea9a3e98275de85a3879b2725a01f5e3274013a
ashiqsultan <ashiq.sultan@iqvia.com>
2022-05-26 18:59:13 +0530

    1344 added redux props to apollo notepad component

--- src/components/views/ApolloNotepad/index.jsx
+++ src/components/views/ApolloNotepad/index.jsx
@@ -1,14 +1,43 @@
 import React, { Fragment, useEffect, useState } from 'react';
+import { connect } from 'react-redux';
 import RichTextEditor from 'apollo-react/components/RichTextEditor';
 import debounce from 'lodash/debounce';
-import { EditorState, convertFromRaw } from 'draft-js';
+import { Editor, EditorState, convertFromRaw } from 'draft-js';
+import { selectNotes } from '../../../redux/selectors';
 
 /**
  * @defaultNote should be in RichText JSON type i.e {block:[],entityMap:{}}
  */
-const ApolloNotepad = ({ defaultNote = null }) => {
+const ApolloNotepad = ({ notes = null }) => {
+  const emptyTextBlock = {
+    blocks: [
+      {
+        key: '55bda',
+        text: 'Initial data',
+        type: 'unstyled',
+        depth: 0,
+        entityRanges: [],
+        data: {}
+      }
+    ],
+    entityMap: {}
+  };
+  const [noteText, setNoteText] = useState(emptyTextBlock);
+
+  const initialEditorState = EditorState.createEmpty();
+  const [editorState, setEditorState] = useState(initialEditorState);
+
+  useEffect(() => {
+    if (notes.size > 0) {
+      const newNotes = JSON.parse(notes.get(0).toJS().noteText);
+      setNoteText(newNotes);
+      setEditorState(EditorState.createWithContent(convertFromRaw(newNotes)));
+    }
+  }, [notes]);
+
   let isFirstLoad = true;
   const handleNotesChange = debounce(value => {
+    setNoteText(value);
     console.log({ isFirstLoad, time: new Date(), value });
     // This function should not save data to db on first load
     if (!isFirstLoad) {
@@ -17,15 +46,21 @@ const ApolloNotepad = ({ defaultNote = null }) => {
     }
     isFirstLoad = false;
   }, 2000);
+
   return (
     <div>
+      <p>{JSON.stringify(noteText)}</p>
       <RichTextEditor
         spellCheck={true}
-        onChange={handleNotesChange}
-        defaultValue={defaultNote}
+        onChange={setNoteText}
+        defaultValue={noteText}
       />
+      <Editor editorState={editorState} onChange={setEditorState} />
     </div>
   );
 };
 
-export default ApolloNotepad;
+const mapStateToProps = state => ({
+  notes: selectNotes(state)
+});
+export default connect(mapStateToProps)(ApolloNotepad);
