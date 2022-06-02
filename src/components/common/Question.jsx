f8db232674970afa458cc899e6162d6424e92c36
Sharma <ankit.sharma3@iqvia.com>
2022-06-02 12:52:54 +0530

    IQVIACBEXU-1364 - Added QuestionHintHtml

--- src/components/common/Question.jsx
+++ src/components/common/Question.jsx
@@ -566,9 +566,10 @@ export class TaskRow extends Component<Props, State> {
                   <Edit className="edit-icon" />
                 </span>
               )}
-                            {questionHint.trim().length > 0 ? (
-                <Tooltip variant="light" title={questionHintHtml.trim().length > 0 ? (        
-                  <div dangerouslySetInnerHTML={ { __html: questionHintHtml } }></div>):(<p>{questionHint}</p>)} placement="top">
+              
+              {questionHint.trim().length > 0 ? (
+                <Tooltip variant="light" title={questionHintHtml.trim().length > 0 ? (
+                  <div dangerouslySetInnerHTML={{ __html: questionHintHtml }}></div>) : (<p>{questionHint}</p>)} placement="top">
                   <IconButton
                     color="primary"
                     style={{ margin: 0 }}
