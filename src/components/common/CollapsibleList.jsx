213d7e2b32f4694bc5b1e9144980cb15a31dffc5
Sharma <ankit.sharma3@iqvia.com>
2022-06-02 12:52:23 +0530

    IQVIACBEXU-1364 - Added QuestionHintHtml

--- src/components/common/CollapsibleList.jsx
+++ src/components/common/CollapsibleList.jsx
@@ -256,6 +256,8 @@ class CollapsibleList extends Component<Props, State> {
                     answerConfiguration={questionConfig.get(
                       'answerConfiguration'
                     )}
+
+                    
                     sfObject={questionConfig.get('sfObject')}
                     sfField={questionConfig.get('sfField')}
                     sectionName={title}
