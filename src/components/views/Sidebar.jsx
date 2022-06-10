08953a6d6dd4a9ae4d2b39cd479180f8e3caada0
Abhishek <abhishek.jha@iqvia.com>
2022-06-10 16:42:39 +0530

    Sidebar logic for active questions

--- src/components/views/Sidebar.jsx
+++ src/components/views/Sidebar.jsx
@@ -362,9 +362,12 @@ class Sidebar extends Component<Props, State> {
                   const sectionNameId = sectionName.toLocaleLowerCase().split(' ').join('-');
                   const questions = section.get('questions');
                   const someQuestionsAreVisible = questions
-                    .valueSeq()
-                    .map(question => question.get('visible', true))
-                    .includes(true);
+                  .valueSeq()
+                  .map(
+                      question =>
+                      question.get('visible', true) && question.get('active', true)
+                  )
+                .includes(true);
 
                   if (someQuestionsAreVisible)
                     return (
