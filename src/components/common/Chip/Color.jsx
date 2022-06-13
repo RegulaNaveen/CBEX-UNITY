248c6f00eb5214b5760ec3d0084817e5b8da87f3
Sharma <ankit.sharma3@iqvia.com>
2022-06-13 16:42:57 +0530

    IQVIACBEXU-1527 - Added the ability to assign color to milestone

---
+++ src/components/common/Chip/Color.jsx
@@ -0,0 +1,19 @@
+export const selectColors = [
+
+    { label: 'Blue', color: '#015ff1' },
+  
+    { label: 'Green', color: '#00c221' },
+  
+    { label: 'Dark Blue', color: '#10558a' },
+  
+    { label: 'Fuchsia', color: '#df216d' },
+  
+    { label: 'Purple', color: '#9e54b0' },
+  
+    { label: 'Orange', color: '#ff9300' },
+  
+    { label: 'Gray', color: '#595959' },
+  
+    { label: 'Black', color: '#000000' },
+  
+  ];
