8931713ee5745c03b95f04ad6f7f9ec204eea9d8
Abhishek <abhishek.jha@iqvia.com>
2022-06-13 11:43:09 +0530

    remove space

--- src/components/screens/Opportunity/Questions.jsx
+++ src/components/screens/Opportunity/Questions.jsx
@@ -348,14 +348,12 @@ class Questions extends Component<Props, State> {
         const someQuestionsAreVisible = questions
         .valueSeq()
         .map(
-          (question) =>
+            question =>
               question.get('visible', true) &&
               (question.get('active', true) ||
                 question.get('isCustomQuestion', true))
         )
         .includes(true);
-
-        
 
         if (someQuestionsAreVisible)
           return (
