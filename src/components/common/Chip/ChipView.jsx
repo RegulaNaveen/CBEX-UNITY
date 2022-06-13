e436069b06a6d3bd6aa9d55faf39b012fb0d9f31
Sharma <ankit.sharma3@iqvia.com>
2022-06-14 00:26:27 +0530

    IQVIACBEXU-1527 - Old + new Opportunity

--- src/components/common/Chip/ChipView.jsx
+++ src/components/common/Chip/ChipView.jsx
@@ -8,43 +8,36 @@ import {selectColors} from './Color';
 const ChipView = ({ label, answer }) => {
   let dynamiclabel = null;
   let color =null;
-  let colorhex = null;
-  
-    if(typeof label === 'object'){
-      const parsedata = JSON.parse(label)
-      label = parsedata.Name;
-      colorhex = parsedata.Color;
-      {selectColors.map((item) => {
-        if(_.isEqual(colorhex, item.color)){ 
-          color= item.label.toLowerCase() }
-          
-      }
-      )}
-  
-      //console.log(dynamiclabel)
-    }
-    else if(typeof label === 'string'){
-      label = label;
-    }
-    {selectColors.map((item) => {
-      if(_.isEqual(colorhex, item.color)){ 
-        color= item.label.toLowerCase() }
-        
-    }
-    )}
-    console.log(color)
+  let diff = /([[])/g
+  if(label.match(diff)){
+console.log(label)
+console.log(JSON.parse(label))
+dynamiclabel = JSON.parse(label)[0].Name
+color = JSON.parse(label)[0].Color
+console.log(color)
+{selectColors.map((item) => {
+  if(_.isEqual(color, item.color)){ 
+    color= item.label.toLowerCase() }
+    
+}
+)}
+  }
+  else {
+    dynamiclabel = label;
+  }
+
   return (
     <>
-      <Tooltip title={label} placement="top">
+      <Tooltip title={dynamiclabel} placement="top">
         <Tag
           Icon={''}
           label={
-            label.split(' ')[0].length <= 11
-              ? label.split(' ')[0]
-              : `${label.split(' ')[0].substr(0, 8)}...`
+            dynamiclabel.split(' ')[0].length <= 11
+              ? dynamiclabel.split(' ')[0]
+              : `${dynamiclabel.split(' ')[0].substr(0, 8)}...`
           }
 
-          variant={color}
+          variant={color || 'blue'}
 
 
         />
