aa2a93dd79d68325e717fcabbcc177a9eaf9adba
Sharma <ankit.sharma3@iqvia.com>
2022-06-20 14:01:35 +0530

    Answers not displaying

--- src/components/common/atoms/inputs/AutoComplete.jsx
+++ src/components/common/atoms/inputs/AutoComplete.jsx
@@ -1,6 +1,7 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import AutocompleteV2 from 'apollo-react/components/AutocompleteV2';
+import Loader from 'apollo-react/components/Loader';
 import { API } from '../../../../constants';
 import { getAccessTokenFromLocalStorage as getAccessToken } from '../../../../SessionHandler';
 
@@ -9,17 +10,46 @@ const Autocomplete = props => {
   const [options, setOptions] = useState([]);
   const [value, setValue] = useState([]);
   const text = String(props?.text)
-    .trimStart()
-    .trimEnd();
+  .trimStart()
+  .trimEnd();
   const previousController = useRef();
   const { disabled } = props;
-
+  let callAccept = false;
+  function extractEmails(str) {
+    let result = String(str).match(
+      /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi
+    );
+    return result ? (result.length ? result[0] : '') : '';
+  }
+  
+  /**
+   * Extracts name from the string format: `Firstname Lastname(name@example.com)`
+   */
+  function extractName(str) {
+    const splirt_array = str.split('(');
+    return splirt_array // check null
+      ? splirt_array.length > 0
+        ? splirt_array[0].trim()
+        : ''
+      : '';
+  }
+  useEffect(() => {
+    if (Boolean(text.length)) {
+      let Val = text.split(',').map(v => {
+        let email = extractEmails(v) || v;
+        let label = extractName(v) || email;
+        return { label, email };
+      });
+      setValue(Val);
+    } else setValue([]);
+  }, [text]);
   const getData = searchTerm => {
     if (previousController.current) {
       previousController.current.abort();
+
     }
-    const controller = new AbortController();
-    const { signal } = controller;
+    var controller = new AbortController();
+    var signal = controller.signal;
     previousController.current = controller;
     try {
       fetch(`${USER_API_URL}/${searchTerm}`, {
@@ -28,46 +58,51 @@ const Autocomplete = props => {
           'x-api-key': API_KEY,
           'x-access-token': getAccessToken()
         }
-      })
+      } )
         .then(response => response.json())
         .then(myJson => {
+          if(callAccept == false) 
+          return
+
+          console.log(`search term: ${searchTerm}, results: `, myJson);
+          console.log(myJson);
           const updatedOptions = myJson.data.map(p => {
-            return {
-              label: `${p.first_name} ${p.last_name} (${p.email})` || '',
-              full_name: `${p.first_name} ${p.last_name}` || ''
-            };
+            return { label: `${p.first_name} ${p.last_name}(${p.email.toLowerCase()})`,
+          full_name: `${p.first_name} ${p.last_name}`};
           });
           setOptions(updatedOptions);
-        })
-        .catch(error => {
-          console.log(error);
         });
     } catch (error) {
       console.error(error);
     }
   };
+
+
+
   const handleChange = (event, newValue) => {
     setValue(newValue);
     const proposaluser = newValue.map(v => {
-      return v.label;
+      return v.label
     });
     if (proposaluser.length == 0) props.onChange(' ', text);
     else props.onChange(proposaluser.join(','), text);
   };
-  const onInputChange = (event, value) => {
+  const onInputChange = _.debounce((event, value) => {
     if (value) {
-      getData(value);
+      callAccept = true;
+      getData(value)
     } else {
+      callAccept = false;
       setOptions([]);
     }
-  };
+  },100);
 
   return (
     <div className={`${disabled ? 'autocomplete-disabled' : 'autocomplete'}`}>
       <AutocompleteV2
         fullWidth
         multiple
-        options={options.length > 0 ? options : []}
+        options={options || []}
         chipColor="white"
         size="small"
         limitChips={50}
