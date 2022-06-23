eddee5b8f3b3b902b0020477b08bc4cd352257d3
Sharma <ankit.sharma3@iqvia.com>
2022-06-23 13:13:35 +0530

    Fixed Tooltip alignment and Autocomplete

--- src/components/common/Question.jsx
+++ src/components/common/Question.jsx
@@ -313,7 +313,7 @@ export class TaskRow extends Component<Props, State> {
     const { setProposalAnswer, proposalId, questionId, userData } = this.props;
     const { selectedDay } = this.state;
     this.setState({ selectedDay: ' ' }, () => {
-      setProposalAnswer(proposalId, questionId, this.state.selectedDay, userData);
+      setProposalAnswer(proposalId, questionId, selectedDay, userData);
       this.trackMatomoEventSubmitAnswer(' ');
     });
   };
@@ -637,7 +637,10 @@ export class TaskRow extends Component<Props, State> {
           >
             {/* questionText */}
             <div style={{ display: 'flex', alignItems: 'flex-start' }}>
-              <div style={{ zIndex: 0, alignSelf: 'center' }}>
+              <div
+                style={{ zIndex: 0, alignSelf: 'center' }}
+                className="questiontext-richtext"
+              >
                 <Typography variant="body2">
                   {questionJSON ? (
                     <RichTextEditor
