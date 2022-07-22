5298de01c1d381830c8c9f9ea9eaceea884777a9
ashiqsultan <ashiq.sultan@iqvia.com>
2022-07-22 16:20:17 +0530

    1194 Added todos in notification api

--- src/api/notification.js
+++ src/api/notification.js
@@ -55,10 +55,8 @@ function fetchNotifications() {
   const email = getUserEmail();
   const config = getAxiosConfig();
   return new Promise((resolve, reject) => {
-    // axios
-    //   .get(`${NOTIFICATION_API_URL}/useremail`, config)
-    //   .then(response => resolve(response.data))
-    //   .catch(err => reject(err));
+    // TODO
+    // Make API call to get all notifications
     resolve(tempDummyData);
   });
 }
@@ -66,10 +64,8 @@ function fetchNotifications() {
 function updateSeenOne(notificationId) {
   const config = getAxiosConfig();
   return new Promise((resolve, reject) => {
-    // axios
-    //   .patch(`${NOTIFICATION_API_URL}/notificationId`, { isSeen: true }, config)
-    //   .then(response => resolve(response.data))
-    //   .catch(err => reject(err));
+    // TODO
+    // Make API call to update one notification as seen
     resolve([]);
   });
 }
@@ -78,10 +74,8 @@ function updateSeenBatch(notificationIds: Array<string>) {
   const config = getAxiosConfig();
   const data = { notificationIds };
   return new Promise((resolve, reject) => {
-    // axios
-    //   .post(`${NOTIFICATION_API_URL}/batch-update-isseen`, data, config)
-    //   .then(response => resolve(response.data))
-    //   .catch(err => reject(err));
+    // TODO
+    // Add api call to update all notifiaction of an user as seen
     resolve([]);
   });
 }
