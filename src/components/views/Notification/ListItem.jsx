33b1a8dbef8832162b9f8b4206e79ff059252e22
ashiqsultan <ashiq.sultan@iqvia.com>
2022-07-28 12:26:36 +0530

    Added Link to Notiication Opportunity No

--- src/components/views/Notification/ListItem.jsx
+++ src/components/views/Notification/ListItem.jsx
@@ -6,8 +6,10 @@ import Typography from 'apollo-react/components/Typography';
 import StatusDotSolid from 'apollo-react-icons/StatusDotSolid';
 import * as notificationActions from '../../../redux/actions/notification-actions';
 import EnvelopeButton from './EnvelopeButton';
+import { useHistory } from 'react-router-dom';
 
 const ListItem = ({ id, url, oppNo, data, isSeen, setSeenOne, createdAt }) => {
+  const history = useHistory();
   const getYesterday = () => {
     let d = new Date();
     d.setDate(d.getDate() - 1);
@@ -37,14 +39,16 @@ const ListItem = ({ id, url, oppNo, data, isSeen, setSeenOne, createdAt }) => {
       <div className='notification-item-content'>
         {/* Header */}
         <div className='notification-item-header'>
-          <Link to={`${url}`}>
-            <Typography
-              variant='body2'
-              className='notification-item-header-title'
-            >
-              {oppNo}
-            </Typography>
-          </Link>
+          <Typography
+            variant='body2'
+            className='notification-item-header-title'
+            onClick={() => {
+              history.push(url);
+              history.go();
+            }}
+          >
+            {oppNo}
+          </Typography>
 
           {/* Envelope Button */}
           <EnvelopeButton isSeen={isSeen} onClick={() => setSeenOne(id)} />
