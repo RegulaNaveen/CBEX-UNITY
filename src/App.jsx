c0876f8421bf2db4310c0e44cacff025111a8167
Sharma <Dheera.Sharma@iqvia.com>
2022-08-03 19:30:21 +0530

    Add Error Boundary to application

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,8 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import PrivateRoute from './PrivateRoute';
-import {
-  LOGIN,
-  PROPOSALS,
-  DASHBOARD,
-  UBUILD,
-  OPPORTUNITYS,
-  PROFILE
-} from './routes';
+import { LOGIN, PROPOSALS, DASHBOARD, UBUILD, OPPORTUNITYS } from './routes';
 import SessionHandler from './SSOSessionHandler';
-import ProfileComponent from './components/screens/Profile';
 import Login from './components/screens/Auth/Login';
 import ProposalComponent from './components/screens/Proposal';
 import OpportunityComponent from './components/screens/Opportunity';
@@ -23,27 +15,30 @@ import UbuildShellComponent from './components/screens/Ubuild';
 import '../styles/App.scss';
 import matomoInstace from './utils/Matomo';
 import SocketContextProvider from './context/SocketContext';
+import ErrorBoundaryComponent from './components/HOC/ErrorBoundary';
 
 const App = () => (
   <Provider store={store}>
     <SocketContextProvider>
       <MatomoProvider value={matomoInstace}>
-        <BrowserRouter>
-          <SessionHandler>
-            <Switch>
-              <Route path="/" exact component={Login} />
-              <Route path={LOGIN} component={Login} />
-              <PrivateRoute path={DASHBOARD} component={DashboardComponent} />
-              <PrivateRoute path={PROPOSALS} component={ProposalComponent} />
-              <PrivateRoute
-                path={OPPORTUNITYS}
-                component={OpportunityComponent}
-              />
-              <PrivateRoute path={UBUILD} component={UbuildShellComponent} />
-              <Redirect to={Login} />
-            </Switch>
-          </SessionHandler>
-        </BrowserRouter>
+        <ErrorBoundaryComponent>
+          <BrowserRouter>
+            <SessionHandler>
+              <Switch>
+                <Route path="/" exact component={Login} />
+                <Route path={LOGIN} component={Login} />
+                <PrivateRoute path={DASHBOARD} component={DashboardComponent} />
+                <PrivateRoute path={PROPOSALS} component={ProposalComponent} />
+                <PrivateRoute
+                  path={OPPORTUNITYS}
+                  component={OpportunityComponent}
+                />
+                <PrivateRoute path={UBUILD} component={UbuildShellComponent} />
+                <Redirect to={Login} />
+              </Switch>
+            </SessionHandler>
+          </BrowserRouter>
+        </ErrorBoundaryComponent>
       </MatomoProvider>
     </SocketContextProvider>
   </Provider>
