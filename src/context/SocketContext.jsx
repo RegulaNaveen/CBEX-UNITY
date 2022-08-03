c230d3cc77c2c7cae5512a047f8626331f0b10c1
ashiqsultan <ashiq.sultan@iqvia.com>
2022-08-03 20:12:38 +0530

    Added timeout functions to retry socket send

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -13,6 +13,7 @@ import {
 import { updateProposalNotesFromWebSocket } from '../redux/actions/notepad-actions';
 import { setNotification } from '../redux/actions/notification-actions';
 import { getUserName, getUserEmail, getUserId } from '../SessionHandler';
+
 const userName = getUserName();
 const userEmail = getUserEmail();
 const userId = getUserId();
@@ -24,14 +25,28 @@ const SocketContextProvider = props => {
   const [socket, setSocket] = useState(null);
   const [OppId, setOppId] = useState(null);
 
+  /**
+   * Checks for socket connection
+   */
+  const isSocketConnected = () => {
+    if (
+      socket?.readyState !== WebSocket.OPEN &&
+      socket?.readyState !== WebSocket.CONNECTING &&
+      socket?.readyState !== 1
+    ) {
+      return false;
+    }
+    return true;
+  };
+
+  /**
+   * Initiates connection only if socket is not connected
+   */
   const initiateConnection = () => {
-    console.log('initiateSocketConnection');
-    if (!socket) {
-      console.log('createSocket');
+    if (!isSocketConnected()) {
       const newSocket = new WebSocket(SOCKET_URL);
 
       newSocket.onopen = event => {
-        console.log('Socket Connected', event);
         if (newSocket) {
           newSocket.send(
             JSON.stringify({
@@ -41,7 +56,7 @@ const SocketContextProvider = props => {
           );
         }
       };
-      console.log('Adding listeners to socket');
+
       const {
         addNewBid,
         getOpportunityInfo,
@@ -56,7 +71,7 @@ const SocketContextProvider = props => {
       // On Message Recieve
       newSocket.addEventListener('message', async response => {
         const data = JSON.parse(response.data);
-        console.log('data.event:', data.event);
+
         switch (data.event) {
           case 'IN_PROGRESS':
             addNewBid(data.data);
@@ -102,20 +117,38 @@ const SocketContextProvider = props => {
     }
   };
 
+  /**
+   * Waits for Socket connection to establish before executing the callback
+   * Retries connection every two second
+   */
+  const waitForSocketConnection = callback => {
+    setTimeout(() => {
+      if (isSocketConnected()) {
+        if (callback instanceof Function) {
+          callback();
+        }
+      } else {
+        waitForSocketConnection(callback);
+      }
+    }, 2000);
+  };
+
+  const sendUpdateConnection = oppId => {
+    socket.send(
+      JSON.stringify({
+        action: 'UPDATE_CONNECTION',
+        body: { oppId }
+      })
+    );
+  };
+
   const updateSocketOppId = oppId => {
     setOppId(oppId);
-    if (socket) {
-      socket.send(
-        JSON.stringify({
-          action: 'UPDATE_CONNECTION',
-          body: { oppId }
-        })
-      );
-    }
+    waitForSocketConnection(() => sendUpdateConnection(oppId));
   };
 
   const disconnectSocket = () => {
-    if (socket) {
+    if (isSocketConnected()) {
       socket.send(
         JSON.stringify({
           action: '$disconnect',
@@ -132,7 +165,8 @@ const SocketContextProvider = props => {
         setSocket,
         initiateConnection,
         updateSocketOppId,
-        disconnectSocket
+        disconnectSocket,
+        isSocketConnected
       }}
     >
       {props.children}
