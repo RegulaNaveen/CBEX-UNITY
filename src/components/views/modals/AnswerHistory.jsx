4418ba64db26b490b1ea3c0c7f818cfb1d392793
TIWARI <rahul.tiwari@iqvia.com>
2022-08-23 20:23:34 +0530

    latest

--- src/components/views/modals/AnswerHistory.jsx
+++ src/components/views/modals/AnswerHistory.jsx
@@ -133,7 +133,7 @@ class AnswerHistory extends Component<Props> {
           .proposalDetails.bidNo;
       }
 
-      const nextAnswer = answers.get(index + 1)
+      let nextAnswer = answers.get(index + 1)
         ? answers.get(index + 1).get('answer')
         : answer;
 
@@ -266,7 +266,7 @@ class AnswerHistory extends Component<Props> {
                 'removed'
               );
             }
-            return `${nextDate} ${newDate}`;
+          return(<>{nextDate} {newDate}</>);
           };
 
           if (questionType === 'select' || questionType === 'select-lookup') {
