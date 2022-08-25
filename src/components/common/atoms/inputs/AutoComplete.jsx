a2a654a933855700f1d273f77bd276e13c6a95db
Kumari <varsha.kumari2@iqvia.com>
2022-08-25 19:10:28 +0530

    Bugfix/2040

--- src/components/common/atoms/inputs/AutoComplete.jsx
+++ src/components/common/atoms/inputs/AutoComplete.jsx
@@ -1,30 +1,28 @@
-import React, { useRef, useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import AutocompleteV2 from "apollo-react/components/AutocompleteV2";
-import Loader from "react-loader-spinner";
-import { API } from "../../../../constants";
-import { getAccessTokenFromLocalStorage as getAccessToken } from "../../../../SessionHandler";
+import React, { useRef, useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import AutocompleteV2 from 'apollo-react/components/AutocompleteV2';
+import { API } from '../../../../constants';
+import { getAccessTokenFromLocalStorage as getAccessToken } from '../../../../SessionHandler';
 
 const { USER_API_URL, API_KEY } = API.PROPOSAL;
-const Autocomplete = (props) => {
+const Autocomplete = props => {
   const [options, setOptions] = useState([]);
   const [value, setValue] = useState([]);
   const [callAccept, setCallAccept] = useState(false);
-  const [count, setCount] = useState(1);
-
   const text = String(props?.text)
     .trimStart()
     .trimEnd();
   const previousController = useRef();
   const { disabled } = props;
+  // let callAccept = false;
 
   function filter() {
-    value.map((row) => {
+    value.map(row => {
       let matched = row.email;
-      options.map((row2) => {
+      options.map(row2 => {
         let matcharray = row2.mail;
         if (matcharray == matched) {
-          const index = options.findIndex((x) => x.mail === matched);
+          const index = options.findIndex(x => x.mail === matched);
           if (index > -1) {
             options.splice(index, 1);
           }
@@ -39,24 +37,24 @@ const Autocomplete = (props) => {
     let result = String(str).match(
       /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi
     );
-    return result ? (result.length ? result[0] : "") : "";
+    return result ? (result.length ? result[0] : '') : '';
   }
 
   /**
    * Extracts name from the string format: `Firstname Lastname(name@example.com)`
    */
   function extractName(str) {
-    const splirt_array = str.split("(");
+    const splirt_array = str.split('(');
     return splirt_array // check null
       ? splirt_array.length > 0
         ? splirt_array[0].trim()
-        : ""
-      : "";
+        : ''
+      : '';
   }
 
   useEffect(() => {
     if (Boolean(text.length)) {
-      let Val = text.split(",").map((v) => {
+      let Val = text.split(',').map(v => {
         let email = extractEmails(v) || v;
         let label = extractName(v) || email;
         return { label, email };
@@ -65,29 +63,29 @@ const Autocomplete = (props) => {
     } else setValue([]);
   }, [text]);
 
-  const getData = (searchTerm) => {
+  const getData = searchTerm => {
     if (previousController.current) {
       previousController.current.abort();
     }
     var controller = new AbortController();
     var signal = controller.signal;
     previousController.current = controller;
+
     try {
       fetch(`${USER_API_URL}/${searchTerm}`, {
         signal,
         headers: {
-          "x-api-key": API_KEY,
-          "x-access-token": getAccessToken(),
-        },
+          'x-api-key': API_KEY,
+          'x-access-token': getAccessToken()
+        }
       })
-        .then((response) => response.json())
-        .then((myJson) => {
-          setCount(myJson.data.length);
+        .then(response => response.json())
+        .then(myJson => {
           if (callAccept === false) return;
-          const updatedOptions = myJson.data.map((p) => {
+          const updatedOptions = myJson.data.map(p => {
             return {
               label: `${p.first_name} ${p.last_name}(${p.email.toLowerCase()})`,
-              mail: `${p.email.toLowerCase()}`,
+              mail: `${p.email.toLowerCase()}`
             };
           });
           setOptions(updatedOptions);
@@ -98,51 +96,53 @@ const Autocomplete = (props) => {
   };
 
   const handleChange = (event, newValue, reason) => {
+    console.log('newValue', newValue);
     setValue(newValue);
-    const proposaluser = newValue.map((v) => {
-      return v.email ? v.label + "(" + v.email + ")" : v.label;
+    const proposaluser = newValue.map(v => {
+      return v.email ? v.label + '(' + v.email + ')' : v.label;
     });
-    if (proposaluser.length === 0) props.onChange(" ", text, reason);
-    else props.onChange(proposaluser.join(","), text, reason);
+    if (proposaluser.length === 0) props.onChange(' ', text, reason);
+    else props.onChange(proposaluser.join(','), text, reason);
   };
 
   const onInputChange = _.debounce((event, value) => {
     if (value) {
       setCallAccept(true);
-      setCount(1);
+      // setCount(1);
       getData(value);
     } else {
       setCallAccept(false);
-      setCount(1);
+      // setCount(1);
       setOptions([]);
     }
   }, 100);
+  const handleBackspace = e => {
+    console.log(e.key);
+    if (e.keyCode === 8) {
+      setCallAccept(false);
+      console.log('1');
+    }
+  };
 
   return (
-    <div className={`${disabled ? "autocomplete-disabled" : "autocomplete"}`}>
+    <div className={`${disabled ? 'autocomplete-disabled' : 'autocomplete'}`}>
       <AutocompleteV2
         fullWidth
         multiple
-        freeSolo={!callAccept}
         options={options || []}
-        chipColor="white"
-        size="small"
+        chipColor='white'
+        size='small'
         limitChips={5}
-        matchFrom="any"
+        disableCloseOnSelect={false}
         value={value}
         onChange={handleChange}
         onInputChange={onInputChange}
-        noOptionsText={
-          options.length == 0 && !count ? (
-            "No matches found"
-          ) : (
-            <Loader type="TailSpin" color="#297DFD" height={40} width={40} />
-          )
-        }
+        noOptionsText='No matches found'
         open={options.length > 0}
-        onFocus={(e) => props.onFocus()}
-        onBlur={(e) => props.onBlur()}
+        onFocus={e => props.onFocus()}
+        onBlur={e => props.onBlur()}
         disabled={disabled || false}
+        onKeyDown={handleBackspace}
       />
     </div>
   );
@@ -151,7 +151,7 @@ const Autocomplete = (props) => {
 Autocomplete.propTypes = {
   disabled: PropTypes.bool.isRequired,
   onFocus: PropTypes.func.isRequired,
-  onBlur: PropTypes.func.isRequired,
+  onBlur: PropTypes.func.isRequired
 };
 
 export default Autocomplete;
