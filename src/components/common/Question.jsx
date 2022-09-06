76c37e003b2414e3755dcb19f46fe6121612bdea
Sharma <ankit.sharma3@iqvia.com>
2022-09-06 14:23:02 +0530

    Font size and box size issue

--- src/components/common/Question.jsx
+++ src/components/common/Question.jsx
@@ -621,7 +621,6 @@ export class TaskRow extends React.PureComponent<Props, State> {
               onBlur={() => this.setSelectRow(false)}
               disabled={checkDisableFlag()}
               onChange={this.handlePropsalChange}
-              placeholder={checkDisableFlag() ? '' : 'Click to answer'}
             />
           </SFAnswerValidationWrapper>
         );
@@ -642,7 +641,6 @@ export class TaskRow extends React.PureComponent<Props, State> {
               multiple={false}
               loading={loading}
               disabled={checkDisableFlag()}
-              placeholder={checkDisableFlag() ? '' : 'Click to answer'}
             />
           </SFAnswerValidationWrapper>
         );
