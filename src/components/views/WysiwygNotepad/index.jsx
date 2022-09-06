6ef8fc4e1d300d1acb208dfffa8fdc2b69286103
Aggarwal <Akash.Aggarwal@iqvia.com>
2022-09-06 15:10:16 +0530

    key attribute added

--- src/components/views/WysiwygNotepad/index.jsx
+++ src/components/views/WysiwygNotepad/index.jsx
@@ -147,6 +147,9 @@ const WysiwygNotepad = ({
     setContent(data);
   }, [json, notes]);
 
+  useEffect(() => {
+    console.log('re render because content changed', content);
+  }, [content]);
   const styleMarks = (blk, map) => {
     console.log('calling styleeeeeeee', blk.entityRanges);
     let tempMarks = [];
@@ -188,7 +191,7 @@ const WysiwygNotepad = ({
     console.log('we have nothing');
     if (_.isEmpty(block.text)) {
       console.log('if nothing');
-      jdata.content.push({ type: 'paragraph' });
+      jdata.content.push({ type: 'paragraph', attrs: { textAlign: 'left' } });
     } else {
       jdata.content.push({
         type: 'paragraph',
@@ -196,7 +199,8 @@ const WysiwygNotepad = ({
           {
             type: 'text',
             marks: _.isEmpty(marks) ? undefined : marks,
-            text: block.text
+            text: block.text,
+            attrs: { textAlign: 'left' }
           }
         ]
       });
@@ -435,9 +439,10 @@ const WysiwygNotepad = ({
     });
     let finalJSON = JSON.parse(JSON.stringify(jdata));
     console.log('final', finalJSON);
+    memoizedSaveDB(finalJSON);
     return finalJSON;
   };
-
+  console.log('content is', content);
   const editor = useEditor(
     {
       extensions: [
@@ -477,6 +482,7 @@ const WysiwygNotepad = ({
       onUpdate: ({ editor }) => {
         const Ejson = editor.getJSON();
         updateNoteInStore();
+        console.log('on update is called', Ejson);
         // send the content to an API here
         memoizedSaveDB(Ejson);
       }
@@ -528,10 +534,14 @@ const WysiwygNotepad = ({
       {notesSocket.wsInstance && (
         <div className="editor-notepad">
           <div>
-            <MenuBar editor={editor} />
+            <MenuBar key={selectedBid.get('id')} editor={editor} />
           </div>
           {isNotesFetched ? (
-            <EditorContent editor={editor} className="editor-scroll" />
+            <EditorContent
+              key={selectedBid.get('id')}
+              editor={editor}
+              className="editor-scroll"
+            />
           ) : (
             <Loader
               type="TailSpin"
