966e11ce69e539c603fcd9ae962d9ef749e03043
Kumari <varsha.kumari2@iqvia.com>
2022-09-09 16:38:51 +0530

    recent modification

--- src/components/screens/Profile/RecentActivity/RecentActivity.jsx
+++ src/components/screens/Profile/RecentActivity/RecentActivity.jsx
@@ -9,104 +9,122 @@ import Cog from 'apollo-react-icons/Cog';
 import { getUnreadNotifications } from '../../../../redux/selectors';
 import ListItem from '../../../views/Notification/ListItem';
 import NoNotification from '../../../views/Notification/NoNotification';
-import { Card } from '@material-ui/core';
+import Card from 'apollo-react/components/Card';
 import MatomoHOC from '../../../HOC/MatomoHOC';
 import * as notificationActions from '../../../../redux/actions/notification-actions';
-import './Recent.css';
-
+import './recentactivity.css';
 const RecentActivity = ({ unreadNotifications, setNotifications, oppNo }) => {
   const [isDrawer, setIsDrawer] = useState(false);
   const [isDrawerOptions, setIsDrawerOptions] = useState(false);
-
   const closeIsDrawerOptions = () => {
     setIsDrawerOptions(false);
   };
   const toggleIsDrawerOptions = () => {
     setIsDrawerOptions(!isDrawerOptions);
   };
-
   useEffect(() => {
     setNotifications();
   }, []);
-
   const notificationCount = useMemo(
     () => (unreadNotifications ? unreadNotifications.length : 0),
     [unreadNotifications]
   );
   return (
     <ProfileLayout>
-      <Grid
-        container
-        item
-        // md={12}
-        // sm={12}
-        // xs={12}
-        // style={{ paddingTop: '1.2em', margin: '0', position: 'fixed' }}
-        spacing={2}
-      >
+      <div>
         <div className="recent-search-field">
-          <Search placeholder="Search recent notifications" fullWidth />
+          <Search placeholder="Search recent notifications" fullwidth />
         </div>
-        <Card
-          interactive
-          className="recent-card"
-          style={{ overflowY: 'scroll' }}
-        >
-          <Grid
-            item
-            xs={6}
-            style={{ marginTop: '15px' }}
-            className="recent-header"
-          >
-            Notifications
-          </Grid>
-          <Grid
-            item
-            xs={6}
-            className="recent-cog-header"
-            onClick={toggleIsDrawerOptions}
-            style={{ marginTop: '12px' }}
-          >
-            <Cog />
-            <DrawerOptions
-              isShow={isDrawerOptions}
-              closeIsDrawerOptions={closeIsDrawerOptions}
-            />
-          </Grid>
-          <div className="recent-content">
-            {notificationCount > 0 ? (
-              unreadNotifications.map(item => {
-                return (
-                  <ListItem
-                    key={item.id}
-                    id={item.id}
-                    url={item.url}
-                    oppNo={item.opportunity_no}
-                    data={item.body}
-                    isSeen={item.read}
-                    createdAt={item.created_date}
-                  />
-                );
-              })
-            ) : (
-              <div className="recent-no-notification">
-                <NoNotification />
-              </div>
-            )}
-          </div>
+        <Card interactive className="recent-card">
+          <table style={{ display: 'flex', flexDirection: 'column' }}>
+            <thead>
+              <tr
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  marginTop: '1em',
+                  paddingLeft: '36px'
+                }}
+              >
+                <th
+                  style={{
+                    alignSelf: 'flex-start',
+
+                    fontWeight: 'bold'
+                  }}
+                >
+                  <Typography
+                    className="card-label"
+                    variant="title1"
+                    gutterBottom
+                  >
+                    Notification
+                  </Typography>
+                </th>
+                <th
+                  style={{
+                    flexGrow: '0',
+                    marginRight: '1em'
+                  }}
+                >
+                  <Typography
+                    className="card-label"
+                    variant="caption"
+                    gutterBottom
+                    onClick={toggleIsDrawerOptions}
+                  >
+                    <Cog />
+                    <DrawerOptions
+                      isShow={isDrawerOptions}
+                      closeIsDrawerOptions={closeIsDrawerOptions}
+                    />
+                  </Typography>
+                </th>
+              </tr>
+            </thead>
+            <tbody className="recent-activity-tab">
+              {notificationCount > 0 ? (
+                unreadNotifications.map(item => {
+                  return (
+                    <tr
+                      style={{
+                        // overflowY: 'auto',
+                        display: 'block',
+                        marginTop: '0.5em',
+                        marginLeft: '0.5em'
+                      }}
+                    >
+                      <ListItem
+                        key={item.id}
+                        id={item.id}
+                        url={item.url}
+                        oppNo={item.opportunity_no}
+                        data={item.body}
+                        isSeen={item.read}
+                        createdAt={item.created_date}
+                      />
+                    </tr>
+                  );
+                })
+              ) : (
+                <div className="recent-no-notification">
+                  <NoNotification />
+                </div>
+              )}
+              {/* </tr> */}
+            </tbody>
+          </table>
         </Card>
-      </Grid>
+      </div>
     </ProfileLayout>
   );
 };
 const mapStateToProps = (state: Map) => ({
   unreadNotifications: getUnreadNotifications(state)
 });
-
 const mapDispatchToProps = {
   setNotifications: notificationActions.setNotification
 };
-
 export default connect(
   mapStateToProps,
   mapDispatchToProps
