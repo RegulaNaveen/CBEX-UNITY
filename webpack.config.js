f74579319464368e79341a825c1e3254b44cd37c
ashiqsultan <ashiq.sultan@iqvia.com>
2022-09-30 10:46:51 +0530

    Removed source map in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -69,7 +69,6 @@ module.exports = env => {
         favicon: `./img/favicon/favicon-${apiEnv.replace(/['"]+/g, '')}.ico`
       }),
       new webpack.DefinePlugin({ 'process.env.API_ENV': apiEnv })
-    ],
-    devtool: 'source-map'
+    ]
   };
 };
