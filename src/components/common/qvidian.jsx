2097313553ef33ea1210f3a6d17dcb6e25aca2cb
u1115451 <ankit.sharma3@iqvia.com>
2022-10-03 14:14:48 +0530

    Fixed qvidian icons not displaying icons for some questions list

--- src/components/common/qvidian.jsx
+++ src/components/common/qvidian.jsx
@@ -16,6 +16,21 @@ export default [
     'Site Strategy-D0S': 'Patient Pathway',
     'Opportunity Overview-N0J': 'Study Design',
     'Win Strategy-AR2': 'IQVIA Position',
-    'Team structure & location-C0X': 'Team Member Location'
+    'Team structure & location-C0X': 'Team Member Location',
+    'Proposal Team-D8U': 'Feasibility Lead',
+    'Proposal Team-I9S': 'Regulatory & Start-Up Lead',
+    'Opportunity Overview-Z7C': 'CRM Number',
+    'Opportunity Overview-T1U': 'Proposal Due Date',
+    'Opportunity Overview-M9D': 'Customers Full Name',
+    'Opportunity Overview-I7N': 'Therapeutic Area',
+    '8959b856-0f49-4c4e-8ec8-f5955320866a': 'Indication',
+    'Opportunity Overview-D3B': 'Study Phase',
+    '7ba722f4-8822-4e64-8086-acc8dbc746ab':
+      'Is this a full or partial virtual trial?',
+    '5c2af12a-f1ff-41a9-bd74-fe2ae340faff': 'Study Start Date',
+    'Opportunity Overview-Z1V': 'Study Compound',
+    'Country Strategy-H7V': 'Country Strategy-H7V',
+    '6bdaa786-a85a-454e-add6-f930ef3ec88a': 'Customers Primary Hot Button',
+    'Proposal Team-L8N': 'Project Leadership, Director'
   }
 ];
