76de59cd41e2ff5b25ee5aec9c3e788122f12c83
Simran Negi <simran.negi@iqvia.com>
2022-10-26 23:55:20 +0530

    approval feature flag introduced

--- src/components/common/atoms/inputs/Tab.jsx
+++ src/components/common/atoms/inputs/Tab.jsx
@@ -1,22 +1,85 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Tab from 'apollo-react/components/Tab';
 import Tabs from 'apollo-react/components/Tabs';
 import Questions from '../../../screens/Opportunity/Questions';
 import Documents from '../../../screens/Opportunity/Documents';
 import Validate from '../../../screens/Opportunity/Validate';
+import featureFlags from '../../../../constants/featureFlags';
+import launchDarkly from '../../../../utils/launchDarkly';
 
 const UnityTab = ({ id, enableValidateTab, selectedView }) => {
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
+  const [approvalsFlag, setApprovalsFlag] = useState(false);
+
   useEffect(() => {
-    if (selectedView && selectedView === 'documents') setValue(1);
+    if (selectedView && selectedView === 'documents') {
+      // eslint-disable-next-line no-unused-expressions
+      approvalsFlag ? setValue(2) : setValue(1);
+    }
   }, [selectedView]);
   const handleChangeTab = (event, value) => {
     setValue(value);
   };
-  const renderTab = v => {
+
+  useEffect(() => {
+    (async () => {
+      const approvalFlag = await launchDarkly(featureFlags.APPROVALS, false);
+      setApprovalsFlag(approvalFlag);
+    })();
+  }, []);
+
+  const renderTab = (v) => {
     if (v) {
       return (
         <>
+          {approvalsFlag ? (
+            <Tabs
+              value={value}
+              onChange={handleChangeTab}
+              truncate
+              className="_question-tab"
+            >
+              <Tab label="Strategy Development" />
+              <Tab label="Approvals" />
+              <Tab label="Documents" />
+              <Tab label="Validate" />
+            </Tabs>
+          ) : (
+            <Tabs
+              value={value}
+              onChange={handleChangeTab}
+              truncate
+              className="_question-tab"
+            >
+              <Tab label="Strategy Development" />
+              <Tab label="Documents" />
+              <Tab label="Validate" />
+            </Tabs>
+          )}
+          <div style={{ padding: 20, paddingTop: 5 }}>
+            {value === 0 && <Questions proposalID={id} />}
+            {value === 1 && approvalsFlag && 'Approvals'}
+            {value === 2 && <Documents />}
+            {value === 3 && <Validate />}
+          </div>
+        </>
+      );
+    }
+
+    return (
+      <>
+        {approvalsFlag ? (
+          <Tabs
+            value={value}
+            onChange={handleChangeTab}
+            truncate
+            className="_question-tab"
+          >
+            <Tab label="Strategy Development" />
+            <Tab label="Approvals" />
+            <Tab label="Documents" />
+          </Tabs>
+        ) : (
           <Tabs
             value={value}
             onChange={handleChangeTab}
@@ -25,31 +88,12 @@ const UnityTab = ({ id, enableValidateTab, selectedView }) => {
           >
             <Tab label="Strategy Development" />
             <Tab label="Documents" />
-            <Tab label="Validate" />
           </Tabs>
-          <div style={{ padding: 20, paddingTop: 5 }}>
-            {value === 0 && <Questions proposalID={id} />}
-            {value === 1 && <Documents />}
-            {value === 2 && <Validate />}
-          </div>
-        </>
-      );
-    }
-
-    return (
-      <>
-        <Tabs
-          value={value}
-          onChange={handleChangeTab}
-          truncate
-          className="_question-tab"
-        >
-          <Tab label="Strategy Development" />
-          <Tab label="Documents" />
-        </Tabs>
+        )}
         <div style={{ padding: 20, paddingTop: 5 }}>
           {value === 0 && <Questions proposalID={id} />}
-          {value === 1 && <Documents />}
+          {value === 1 && approvalsFlag && 'Approvals'}
+          {value === 2 && <Documents />}
         </div>
       </>
     );
