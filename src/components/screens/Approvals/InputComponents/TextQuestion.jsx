5b7bdbbc82add726f0a0455e5afe3dfc3b93efa9
ashiqsultan <ashiq.sultan@iqvia.com>
2022-11-10 15:06:32 +0530

    Added answer save functions for Text Answers

--- src/components/screens/Approvals/InputComponents/TextQuestion.jsx
+++ src/components/screens/Approvals/InputComponents/TextQuestion.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import isObject from 'lodash/isObject';
 import has from 'lodash/has';
+import isEqual from 'lodash/isEqual';
+import isEmpty from 'lodash/isEmpty';
 import CustomApolloRichText from '../../../common/CustomApolloRichText';
 import { parseStringifyJson } from '../../../../utils/helpers';
+import { setProposalAnswerData } from '../../../../redux/actions/proposal-actions';
 
 // Function to converted Answer String
 const getConvertedAnsString = str =>
   !String(str).trim() ? '' : String(str).trim();
 
-const TextQuestion = ({ question, lastAnswer }) => {
+const TextQuestion = ({ question, lastAnswer, userData, socketContext }) => {
+  const dispatch = useDispatch();
+  const answerValue = lastAnswer.answer || '';
   const formattedAnswer =
     has(lastAnswer, 'formattedAnswer') && lastAnswer.formattedAnswer;
 
@@ -22,13 +28,60 @@ const TextQuestion = ({ question, lastAnswer }) => {
     value: { blocks: [] }
   };
 
+  const handleRichTextChange = editorData => {
+    const { proposalId, questionId } = question;
+    const { value, html, text } = editorData;
+
+    const editorText = text.trim() || ' ';
+
+    dispatch(
+      setProposalAnswerData(
+        socketContext,
+        proposalId,
+        questionId,
+        String(editorText),
+        userData,
+        {
+          value,
+          html
+        }
+      )
+    );
+  };
+
   const richtextProps = {
-    richTextString: getConvertedAnsString(lastAnswer),
+    richTextString: getConvertedAnsString(answerValue),
     richTextVal: richTextData.value,
     richTextHtml: richTextData.html,
     enableFocus: true,
     isEditable: false,
-    disabled: false
+    disabled: false,
+    onBlur: data => {
+      let saveDate = false;
+      const previousAnsText = getConvertedAnsString(answerValue).trim();
+
+      // save the formatting change
+      if (
+        !isEqual(richTextData.value, data.value) &&
+        !isEmpty(data.text.trim())
+      )
+        if (
+          isEmpty(richTextData.value?.blocks) &&
+          lastAnswer?.answer === data.value?.blocks[0]?.text
+        )
+          saveDate = false;
+        else saveDate = true;
+      // save the data if we see any text difference.
+      else if (previousAnsText !== data.text.trim() && data.text.trim() !== '')
+        saveDate = true;
+      // save the data if user removes the whole answer.
+      else if (previousAnsText !== '' && data.text.trim() === '')
+        saveDate = true;
+
+      if (saveDate) {
+        handleRichTextChange(data);
+      }
+    }
   };
   return (
     <>
