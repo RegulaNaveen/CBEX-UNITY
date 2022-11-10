b670730256326c9a1cbafd0a097a63f9c41daa15
ashiqsultan <ashiq.sultan@iqvia.com>
2022-11-10 12:36:29 +0530

    added answer update function for Number answer type

--- src/components/screens/Approvals/InputComponents/NumberQuestion.jsx
+++ src/components/screens/Approvals/InputComponents/NumberQuestion.jsx
@@ -1,13 +1,71 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useDispatch } from 'react-redux';
+import isEmpty from 'lodash/isEmpty';
 import TextArea from '../../../common/atoms/inputs/TextArea';
+import { setProposalAnswerData } from '../../../../redux/actions/proposal-actions';
+import { SocketContext } from '../../../../context/SocketContext';
+import {
+  getUserName,
+  getUserEmail,
+  getUserId
+} from '../../../../SessionHandler';
 
 const NumberQuestion = ({ question, lastAnswer }) => {
+  const context = useContext(SocketContext);
+  const dispatch = useDispatch();
+
+  const handleTextChange = (textValue, lastAnswer, editorData) => {
+    const { proposalId, questionId } = question;
+    const userData = {
+      email: getUserName(),
+      name: getUserEmail(),
+      role: getUserId()
+    };
+    const s1 = textValue
+      .trim()
+      .split(' ')
+      .filter(v => v.trim().length > 0);
+    const s2 = lastAnswer
+      .trim()
+      .split(' ')
+      .filter(v => v.trim().length > 0);
+
+    if (!isEmpty(textValue.replace(/\r?\n|\r| /g, ''))) {
+      if (
+        s1.length !== s2.length ||
+        s1.join(' ').trim() !== s2.join(' ').trim()
+      ) {
+        dispatch(
+          setProposalAnswerData(
+            context,
+            proposalId,
+            questionId,
+            String(textValue).trim(),
+            userData,
+            editorData
+          )
+        );
+      }
+    } else if (!textValue.trim() && lastAnswer.trim()) {
+      dispatch(
+        setProposalAnswerData(
+          context,
+          proposalId,
+          questionId,
+          ' ',
+          userData,
+          editorData
+        )
+      );
+    }
+  };
   return (
     <>
       <TextArea
         className="proposal-text-area"
         type="number"
         value={lastAnswer.answer}
+        onBlur={handleTextChange}
       />
     </>
   );
