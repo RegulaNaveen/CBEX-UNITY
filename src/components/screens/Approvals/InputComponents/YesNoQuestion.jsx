ebeb256547ed3d257ddd1e84231ea3a3a844409e
ashiqsultan <ashiq.sultan@iqvia.com>
2022-11-11 11:47:39 +0530

    Added answer change fun for yes no answer types

--- src/components/screens/Approvals/InputComponents/YesNoQuestion.jsx
+++ src/components/screens/Approvals/InputComponents/YesNoQuestion.jsx
@@ -1,19 +1,41 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { setProposalAnswerData } from '../../../../redux/actions/proposal-actions';
 import YNDropdown from '../../../common/atoms/inputs/Dropdown';
 
-const YesNoQuestion = ({ question }) => {
-  const optionsYN = ['Yes', 'No'];
-  return (
-    <>
-      <YNDropdown
-        items={optionsYN}
-        disabled={false}
-        questionId={question.questionId}
-        // onClick={val => this.onClickChange(val, answerValue)}
-        // value={answerValue}
-      />
-    </>
-  );
+const YesNoQuestion = ({ question, lastAnswer, userData, socketContext }) => {
+  try {
+    const dispatch = useDispatch();
+    const changeHandler = (selectedValue: string, lastAnswer: string) => {
+      const { proposalId, questionId } = question;
+      if (lastAnswer !== selectedValue) {
+        dispatch(
+          setProposalAnswerData(
+            socketContext,
+            proposalId,
+            questionId,
+            selectedValue,
+            userData
+          )
+        );
+      }
+    };
+    const optionsYN = ['Yes', 'No'];
+    return (
+      <>
+        <YNDropdown
+          items={optionsYN}
+          disabled={false}
+          questionId={question.questionId}
+          value={lastAnswer.answer}
+          onClick={val => changeHandler(val, lastAnswer.answer)}
+        />
+      </>
+    );
+  } catch (error) {
+    console.error(error);
+    return <p>Error rendering y/n question</p>;
+  }
 };
 
 export default YesNoQuestion;
