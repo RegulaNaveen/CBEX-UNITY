5f6c1e9c7be765e87f068cb2694eb195318c7272
ashiqsultan <ashiq.sultan@iqvia.com>
2022-11-29 18:22:30 +0530

    Added styles for approval text questions

--- src/components/screens/Approvals/InputComponents/TextQuestion.jsx
+++ src/components/screens/Approvals/InputComponents/TextQuestion.jsx
@@ -94,7 +94,12 @@ const TextQuestion = ({
     }
   };
 
-  return <CustomApolloRichText {...richtextProps} />;
+  return (
+    <CustomApolloRichText
+      className="approval-text-question"
+      {...richtextProps}
+    />
+  );
 };
 
 TextQuestion.defaultProps = {
