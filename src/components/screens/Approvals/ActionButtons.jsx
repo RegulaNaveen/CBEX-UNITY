375d23159b82aaeda090913e98fc888a96979a0f
TIWARI <rahul.tiwari@iqvia.com>
2022-11-30 19:30:32 +0530

    latest

--- src/components/screens/Approvals/ActionButtons.jsx
+++ src/components/screens/Approvals/ActionButtons.jsx
@@ -56,8 +56,22 @@ const ActionButtons = ({ sectionId, trackEvent, eventCategories }) => {
     trackEvent({
       category: eventCategories.crmNo,
       action: `Approval ${action}`,
-      name: `Approval: ${action}: (${ApprovalSectionTitle}) (${ArchivedData.length -
-        1})`,
+      name: `Approval: ${action}: ${ApprovalSectionTitle} ${ArchivedData.length +
+        1}`,
+      customDimensions: [
+        {
+          id: 1,
+          value: JSON.stringify({
+            proposalDetail,
+            aprovaldata
+          })
+        }
+      ]
+    });
+    trackEvent({
+      category: eventCategories.crmNo,
+      action: `Approval ${action}`,
+      name: `Approval: Duplicate Count: ${ArchivedData.length - 1}`,
       customDimensions: [
         {
           id: 1,
@@ -77,8 +91,8 @@ const ActionButtons = ({ sectionId, trackEvent, eventCategories }) => {
       trackEvent({
         category: eventCategories.crmNo,
         action: `Approval: creation`,
-        name: `Approval Answer: creation: (${ApprovalSectionTitle}) (${ArchivedData.length +
-          1})`,
+        name: `Approval Answer: creation: ${ApprovalSectionTitle} ${ArchivedData.length +
+          2}`,
         customDimensions: [
           {
             id: 1,
@@ -117,8 +131,8 @@ const ActionButtons = ({ sectionId, trackEvent, eventCategories }) => {
     trackEvent({
       category: eventCategories.crmNo,
       action: `Approval ${action}`,
-      name: `Approval: ${action}: (${ApprovalSectionTitle}) (${ArchivedData.length +
-        1})`,
+      name: `Approval: ${action}: ${ApprovalSectionTitle} ${ArchivedData.length +
+        1}`,
       customDimensions: [
         {
           id: 1,
