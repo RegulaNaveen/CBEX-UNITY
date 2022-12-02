5baa1485d0a0fa78f24f9b29382d3fe3cfcf06f6
ashiqsultan <ashiq.sultan@iqvia.com>
2022-12-02 17:34:20 +0530

    Added question unlock to other answer types

--- src/components/screens/Approvals/InputComponents/CheckBoxQuestion.jsx
+++ src/components/screens/Approvals/InputComponents/CheckBoxQuestion.jsx
@@ -39,28 +39,28 @@ const CheckBoxQuestion = ({
       finalOptions = getCountryOptions();
     }
 
-    const changeHandler = textValue => {
-      const { proposalId, questionId } = question;
-
-      dispatch(
-        setProposalAnswerData(
-          socketContext,
-          proposalId,
-          questionId,
-          textValue,
-          userData
-        )
-      );
-      trackMatomoEventSubmitAnswer(textValue);
+    const changeHandler = async textValue => {
+      try {
+        const { proposalId, questionId } = question;
+        await dispatch(
+          setProposalAnswerData(
+            socketContext,
+            proposalId,
+            questionId,
+            textValue,
+            userData
+          )
+        );
+        questionUnlockWrapper(question?.questionId);
+        trackMatomoEventSubmitAnswer(textValue);
+      } catch (error) {
+        console.error(error);
+        questionUnlockWrapper(question?.questionId);
+      }
     };
 
     return (
-      <div
-        className="checkboxtype"
-        // onFocus={() => {
-        //   questionLockWrapper(question?.questionId);
-        // }}
-      >
+      <div className="checkboxtype">
         <CheckBoxQuestions
           answerValue={answerValue}
           finalOptions={finalOptions}
@@ -68,12 +68,12 @@ const CheckBoxQuestion = ({
           onOpen={() => {}}
           onClose={() => {}}
           onChange={e => changeHandler(e)}
-          // onFocus={() => {
-          //   questionLockWrapper(question?.questionId);
-          // }}
-          // onBlur={() => {
-          //   questionUnlockWrapper(question?.questionId);
-          // }}
+          onFocus={() => {
+            questionLockWrapper(question?.questionId);
+          }}
+          onBlur={() => {
+            questionUnlockWrapper(question?.questionId);
+          }}
         />
       </div>
     );
