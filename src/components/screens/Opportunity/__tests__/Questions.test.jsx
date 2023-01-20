d1ce16c8f589f6e7b180000ba059f306871b1424
TIWARI <rahul.tiwari@iqvia.com>
2023-01-20 19:24:05 +0530

    latest tets case

--- src/components/screens/Opportunity/__tests__/Questions.test.jsx
+++ src/components/screens/Opportunity/__tests__/Questions.test.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 /* eslint-disable no-unused-vars */
 /* eslint-disable prefer-destructuring */
 /* eslint-disable object-shorthand */
@@ -6,8 +7,14 @@
 /**
  * @jest-environment jsdom
  */
-import React from 'react';
-import { cleanup, fireEvent, render } from '@testing-library/react';
+import React, { Suspense } from 'react';
+import {
+  cleanup,
+  fireEvent,
+  render,
+  screen,
+  waitFor
+} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { Provider } from 'react-redux';
 import { configure, shallow, mount } from 'enzyme';
@@ -19,8 +26,13 @@ import { createMemoryHistory } from 'history';
 import { BrowserRouter, Router } from 'react-router-dom';
 import Questions from '../Questions';
 import data from './mockdata/question.json';
-import Sidebar from '../../../views/Sidebar';
+import lazyWithRetry from '../../../../utils/lazy';
 
+const Sidebar = React.lazy(() =>
+  lazyWithRetry(() =>
+    import(/* webpackChunkName: "Sidebar" */ '../../../views/Sidebar')
+  )
+);
 configure({ adapter: new Adapter() });
 
 const filterDataMap = {
@@ -104,7 +116,7 @@ data.proposal.questionsFilter.milestoneGroup = Map(
 );
 data.proposal.questionsFilter = Map(data.proposal.questionsFilter);
 data.setQuestion = Map(data.setQuestion);
-data.sidebar = Map(data.sidebar);
+data.sidebar = Map(data.Sidebar);
 data.proposal = Map(data.proposal);
 data.ssoAuth = Map(data.ssoAuth);
 data.eventCategories.pd = jest.fn();
@@ -178,7 +190,9 @@ describe('Questions component', () => {
         <Router history={history}>
           <Provider store={store}>
             <Questions {...initalstate}>
-              <Sidebar {...initalstate} />
+              <Suspense fallback={<p>Loading...</p>}>
+                <Sidebar {...initalstate} />
+              </Suspense>
             </Questions>
           </Provider>
         </Router>
@@ -255,6 +269,128 @@ describe('Questions component', () => {
     expect(queryByTestId('selectedbid-testid')).toBeInTheDocument();
     fireEvent.click(queryByTestId('selectedbid-testid'));
     expect(queryByTestId('question-model-testid')).toBeInTheDocument();
+  });
+
+  test('Questions component expand all render', async () => {
+    const location = window.location;
+    delete window.location;
+    window.location = {
+      ...location,
+      reload: jest.fn()
+    };
+    global.ResizeObserver = jest.fn().mockImplementation(() => ({
+      observe: jest.fn(),
+      unobserve: jest.fn(),
+      disconnect: jest.fn()
+    }));
+    const { getByText } = await render(
+      <BrowserRouter>
+        <Router history={history}>
+          <Provider store={store}>
+            <Questions {...initalstate} />
+          </Provider>
+        </Router>
+      </BrowserRouter>
+    );
+    fireEvent.change(getByText('Expand All'));
+  });
+
+  test('Questions component filter button render', async () => {
+    const location = window.location;
+    delete window.location;
+    window.location = {
+      ...location,
+      reload: jest.fn()
+    };
+    global.ResizeObserver = jest.fn().mockImplementation(() => ({
+      observe: jest.fn(),
+      unobserve: jest.fn(),
+      disconnect: jest.fn()
+    }));
+    const { findByText, getByText } = await render(
+      <BrowserRouter>
+        <Router history={history}>
+          <Provider store={store}>
+            <Questions {...initalstate} />
+          </Provider>
+        </Router>
+      </BrowserRouter>
+    );
+    await fireEvent.click(getByText('Filter'));
+    const filterelem = await findByText('Filters');
+    const clearbtn = await findByText('Clear All');
+    expect(filterelem).toBeInTheDocument();
+    expect(clearbtn).toBeInTheDocument();
+    await fireEvent.click(getByText('Clear All'));
+  });
+
+  test('Questions component mark NA render', async () => {
+    const location = window.location;
+    delete window.location;
+    window.location = {
+      ...location,
+      reload: jest.fn()
+    };
+    global.ResizeObserver = jest.fn().mockImplementation(() => ({
+      observe: jest.fn(),
+      unobserve: jest.fn(),
+      disconnect: jest.fn()
+    }));
+    const { findByText, getByText } = await render(
+      <BrowserRouter>
+        <Router history={history}>
+          <Provider store={store}>
+            <Questions {...initalstate} />
+          </Provider>
+        </Router>
+      </BrowserRouter>
+    );
+    await fireEvent.click(getByText('Mark N/A'));
+    await fireEvent.click(getByText('Filter'));
+    expect(await findByText('Responsible')).toBeInTheDocument();
+    expect(await findByText('Informed')).toBeInTheDocument();
+  });
+
+  test('Questions component Sidebar component render', async () => {
+    const location = window.location;
+    delete window.location;
+    window.location = {
+      ...location,
+      reload: jest.fn()
+    };
+    global.ResizeObserver = jest.fn().mockImplementation(() => ({
+      observe: jest.fn(),
+      unobserve: jest.fn(),
+      disconnect: jest.fn()
+    }));
+    initalstate.sidebar = initalstate.sidebar.toJS();
+    initalstate.sidebar.isOpen = true;
+    initalstate.sidebar = Map(initalstate.sidebar);
+    const { queryByTestId, debug, getByText } = await render(
+      <BrowserRouter>
+        <Router history={history}>
+          <Provider store={store}>
+            <Questions {...initalstate}>
+              <Suspense fallback={<p>Loading...</p>}>
+                <Sidebar {...initalstate} />
+              </Suspense>
+            </Questions>
+          </Provider>
+        </Router>
+      </BrowserRouter>
+    );
+    await waitFor(async () => {
+      debug();
+      const sidebaricon_newQuestionAdd = await queryByTestId(
+        'sidebar-panel-testid'
+      );
+      await fireEvent.click(sidebaricon_newQuestionAdd);
+      await fireEvent.click(queryByTestId('expandall-testid'));
+      await fireEvent.click(queryByTestId('tab-testid'));
+      expect(
+        getByText('Opportunity Information from CRM (for Team review)')
+      ).toBeInTheDocument();
+    });
   });
 
   afterAll(cleanup);
