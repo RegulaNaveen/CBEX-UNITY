90a8b7899062ec07cd1d6edad399edcc1bb42492
Keerthiprasath <keerthiprasath.chandran@iqvia.com>
2023-02-06 20:46:01 +0530

    bugfix/IQVIACBEXU-3711 approvals question order maintained on email template

--- src/components/common/atoms/inputs/Tab.jsx
+++ src/components/common/atoms/inputs/Tab.jsx
@@ -106,7 +106,7 @@ const UnityTab = ({
 
   const { trackEvent } = useMatomo();
 
-  const notepadPanelRef = useRef(null);
+  const panelRef = useRef(null);
 
   const minPixelToExclude = 20;
   const notepadMinWidthPx =
@@ -202,17 +202,21 @@ const UnityTab = ({
   }, [selectedView, approvalsFlag, showApprovalTab]);
 
   useEffect(() => {
-    if (currentSearchResult !== null && notepadPanelRef.current !== null) {
-      if (currentSearchResult.searchIndex === NOTEPAD_UI_ID) {
+    if (currentSearchResult !== null && panelRef.current !== null) {
+      if (
+        currentSearchResult.vTab !== null &&
+        currentSearchResult.vTab >= 0 &&
+        currentSearchResult.vTab <= 2
+      ) {
         if (!isNotepadOpen) {
           // by inspecting DOM, found there is only one button element inside Panel component hence choosing first button
-          const toggleButton = notepadPanelRef.current.children[0].getElementsByTagName(
+          const toggleButton = panelRef.current.children[0].getElementsByTagName(
             'button'
           )[0];
           toggleButton.click();
         }
         setTimeout(() => {
-          notepadPanelRef.current.scrollIntoView({
+          panelRef.current.scrollIntoView({
             behaviour: 'smooth',
             block: 'center',
             inline: 'nearest'
@@ -221,7 +225,7 @@ const UnityTab = ({
         }, 500);
       }
     }
-  }, [dispatch, notepadPanelRef, currentSearchResult, isNotepadOpen]);
+  }, [dispatch, panelRef, currentSearchResult, isNotepadOpen]);
 
   const winLocationSearch = window.location.search;
   const handleChangeTab = (event, val) => {
@@ -258,7 +262,7 @@ const UnityTab = ({
   const renderVerticleTabsComponent = activeVerticleTab => {
     if (activeVerticleTab === 'showQuestionsForCustomerTab') {
       return (
-        <div id="panel-notepad" style={{ borderRadius: '5px' }}>
+        <div id="panel-notepad" style={{ borderRadius: '5px' }} ref={panelRef}>
           <Panel
             minWidth={notepadMinWidthPx}
             maxWidth={notepadMaxWidthPx}
@@ -303,7 +307,7 @@ const UnityTab = ({
             'show-highlight':
               currentSearchResult !== null && currentSearchResult.vTab === 1
           })}
-          ref={notepadPanelRef}
+          ref={panelRef}
         >
           <Panel
             minWidth={notepadMinWidthPx}
@@ -376,7 +380,7 @@ const UnityTab = ({
     }
     if (activeVerticleTab === 'proposalteamtab') {
       return (
-        <div id="panel-notepad" style={{ borderRadius: '5px' }}>
+        <div id="panel-notepad" style={{ borderRadius: '5px' }} ref={panelRef}>
           <Panel
             minWidth={notepadMinWidthPx}
             maxWidth={notepadMaxWidthPx}
