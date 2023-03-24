aea2657c8298ed2cae2f493b7b8b0f259a9dd89f
tapaskdhar <tapas.dhar@iqvia.com>
2023-03-24 16:33:43 +0530

    removed unnecessary call Notepad component on sidebar open

--- src/components/views/Sidebar.jsx
+++ src/components/views/Sidebar.jsx
@@ -347,11 +347,7 @@ class Sidebar extends Component<Props, State> {
                   }}
                 />
               </Tooltip>
-              {isOpen && (
-                <div style={{ display: 'none' }}>
-                  <NotepadWrapper key={id} />
-                </div>
-              )}
+
               {MANUAL_REFRESH && (
                 <Tooltip title="Refresh Proposal Sources" placement="right">
                   <SyncIcon
