d63b22573d855be0392995b0ac41fe30ce9b589c
TIWARI <rahul.tiwari@iqvia.com>
2023-03-24 11:04:58 +0530

    latest

--- src/components/common/atoms/inputs/CheckBoxQuestions.jsx
+++ src/components/common/atoms/inputs/CheckBoxQuestions.jsx
@@ -163,6 +163,7 @@ const CheckBoxQuestions = (props: Props) => {
     if (forceBlur) {
       const popovers = document.getElementsByClassName('MuiPopover-root');
       popovers[0].children[0].click();
+      setFocus(false);
       onClose();
     }
   }, [forceBlur]);
