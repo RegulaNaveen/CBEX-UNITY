72d9d23c4782ade50fc828b7ba63d58b83a34709
Varsha <varsha.kumari2@iqvia.com>
2023-04-17 11:08:32 +0530

    bid cost detials modification

--- src/components/common/BidCostDetails.jsx
+++ src/components/common/BidCostDetails.jsx
@@ -18,6 +18,7 @@ const BidCostDetails = () => {
   if (stage.length > 0) {
     stage = stage[0].answers[0]?.answer;
     stage = Number(stage.substring(0, 2));
+    console.log('stage', stage);
   }
   const [bidCostValue, setBidCostValue] = useState({
     bidValue: '',
@@ -41,8 +42,11 @@ const BidCostDetails = () => {
   const options2 = { currency: 'USD' };
   const numberFormat2 = new Intl.NumberFormat('en-US', options2);
 
-  const bidVal = numberFormat2.format(bidCostValue?.bidValue);
-
+  // const bidVal = numberFormat2.format(bidCostValue?.bidValue);
+  const bidVal =
+    bidCostValue?.bidValue === ''
+      ? ''
+      : numberFormat2.format(bidCostValue?.bidValue);
   const options3 = { currency: 'USD' };
   const numberFormat3 = new Intl.NumberFormat('en-US', options3);
 
@@ -53,15 +57,17 @@ const BidCostDetails = () => {
       [stage >= 4 && bidCostValue?.bidValue === ''
         ? 'Opportunity Amount: '
         : 'Total Bid Value: ']:
-        (stage >= 4 && bidCostValue?.bidValue === ''
+        stage >= 4 && bidCostValue?.bidValue === ''
           ? bidCostValue?.amount
-          : bidVal) || 'N/A'
+          : bidVal
+          ? `USD ${bidVal}`
+          : 'N/A'
     },
 
     { 'Bottom Line Labor Discount: ': bottomLineVal },
     { 'Budget Tools: ': bidCostValue?.budgetTools }
   ];
-
+  console.log('newBidItem', newBidItem);
   useEffect(() => {
     let bidValue = '';
     let bottomLine = '';
@@ -103,11 +109,26 @@ const BidCostDetails = () => {
             return (
               <div className="bid-cost_details-item" key={key}>
                 <h3>{Object.keys(item)[0]}</h3>
-
+                {/* {console.log('first', Object.values(item)[0])}; */}
                 <i>
-                  {Object.values(item)[0] === ''
+                  {/* {Object.values(item)[0] === ''
                     ? 'N/A'
-                    : Object.values(item)[0]}
+                    : Object.values(item)[0]} */}
+                  {Object.keys(item)[0].trim() === 'Amount' ? (
+                    <i>{Object.values(item)[0] || 0}</i>
+                  ) : (
+                    <i>
+                      {[
+                        'Budget Tools:',
+                        'Bottom Line Labor Discount:',
+                        'Total Bid Value:'
+                      ].includes(Object.keys(item)[0].trim()) &&
+                      (Object.values(item)[0].trim() === '' ||
+                        Object.values(item)[0].trim() === '0')
+                        ? 'N/A'
+                        : Object.values(item)[0]}
+                    </i>
+                  )}
                 </i>
               </div>
             );
