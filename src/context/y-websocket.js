1e022be15f7da6b71c23884ee280105df484e1ff
Keerthiprasath <keerthiprasath.chandran@iqvia.com>
2023-05-03 21:59:00 +0530

    bugfix/IQVIACBEXU-4114 notepad sync fail handled

--- src/context/y-websocket.js
+++ src/context/y-websocket.js
@@ -147,6 +147,9 @@ const setupWS = provider => {
       if (event.data === 'refresg=') {
         return;
       }
+      if (event.data === 'sync/failec=') {
+        provider.emit('sync_failed', []);
+      }
       provider.wsLastMessageReceived = time.getUnixTime();
       const encoder = readMessage(
         provider,
