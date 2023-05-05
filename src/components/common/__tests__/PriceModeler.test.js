ad3a3fab76484eab89a43c2c1966c83182bb785b
Sushil Munda <sushil.munda@iqvia.com>
2023-05-05 10:04:22 +0530

    pricemodeler popover unit test

--- src/components/common/__tests__/PriceModeler.test.js
+++ src/components/common/__tests__/PriceModeler.test.js
@@ -15,18 +15,37 @@ describe('render the pricemodeler component', () => {
     });
 
     test('displays estimated price', () => {
-        const mockPriceModuler = {
-            cost: '1000'
-        };
+        const priceModeler = {
+            cost: '1000',
+            therapeutic: 'Oncology',
+            sites: '10',
+            phase: 'Phase 2',
+            patients: '100',
+            regions: 'US'
+          };
         jest.mock('react-redux', () => ({
-            useSelector: jest.fn().mockImplementation(callback => callback(mockPriceModuler))
+            useSelector: jest.fn().mockImplementation(callback => callback(priceModeler))
         }));
-        const { getByText } = render(
+        const { getByText, getByTestId } = render(
             <Provider store={store}>
                 <PriceModeler />
             </Provider>
         );
         expect(getByText('Price Modeler Estimate')).toBeInTheDocument();
+        expect(getByTestId("price-modeler-icon")).toBeInTheDocument();
+    });
+
+    test('test click of the icon button', () => {
+        const DEFAULT_TEXT = "The fields listed below are required for an estimate to be displayed. Excludes investigator grants, vendor costs, and other expenses";
+        const { getByTestId, getByText } = render(
+            <Provider store={store}>
+                <PriceModeler />
+            </Provider>
+        );
+
+        const iconButton = getByTestId("price-modeler-icon");
+        fireEvent.click(iconButton);
+        expect(getByText(DEFAULT_TEXT)).toBeInTheDocument();
     });
 
 });
