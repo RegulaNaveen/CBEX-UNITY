37f6a6e220d8c7392a855be66e3c8d77b0401a50
TIWARI <rahul.tiwari@iqvia.com>
2023-05-16 12:27:47 +0530

    latest

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,6 @@ import ApolloThemeProvider from 'apollo-react/utils/ApolloThemeProvider';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(
