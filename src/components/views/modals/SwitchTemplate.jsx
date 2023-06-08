cf832ccd80122ccf546d6d0236f241b3cc51d570
Keerthiprasath <keerthiprasath.chandran@iqvia.com>
2023-06-08 16:23:17 +0530

    switch template modal UI fix update

--- src/components/views/modals/SwitchTemplate.jsx
+++ src/components/views/modals/SwitchTemplate.jsx
@@ -92,7 +92,7 @@ const SwitchTemplate = ({
             onClick: switchTempBtnClickHandler
           }
         ]}
-        style={{ maxWidth: 558 }}
+        // style={{ maxWidth: 558 }}
         {...props}
       >
         {/* Modal Loading */}
