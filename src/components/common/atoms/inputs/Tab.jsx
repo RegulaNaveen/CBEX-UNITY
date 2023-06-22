faef6d6e6bd28a741e8c6098526ffc9f74906ee6
TIWARI <rahul.tiwari@iqvia.com>
2023-06-22 20:27:47 +0530

    latest

--- src/components/common/atoms/inputs/Tab.jsx
+++ src/components/common/atoms/inputs/Tab.jsx
@@ -575,22 +575,6 @@ const UnityTab = ({
     }
   }, [newTab]);
 
-  useEffect(() => {
-    if (
-      switchTempStatus &&
-      switchTempStatus === 'success' &&
-      tabLoaded &&
-      tabStatus
-    ) {
-      setTimeout(() => {
-        const className = '._question-tab > div > div > button:nth-child(1)';
-        if (document && document.querySelector(className)) {
-          document.querySelector(className).click();
-        }
-      }, 7000);
-    }
-  }, [switchTempStatus, tabStatus, newTab]);
-
   const winLocationSearch = window.location.search;
   const handleChangeTab = (event, val) => {
     const selectView = new URLSearchParams(winLocationSearch);
