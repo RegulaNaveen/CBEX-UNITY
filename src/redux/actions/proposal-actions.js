0f7eb1760059062e9b49e597fdc5c4c4057a437b
Keerthiprasath <keerthiprasath.chandran@iqvia.com>
2023-07-10 14:16:53 +0530

    [IQVIACBEXU-4213] UI fixes

--- src/redux/actions/proposal-actions.js
+++ src/redux/actions/proposal-actions.js
@@ -1800,7 +1800,7 @@ export const updateNextMilestone = (oppNumber, nextMilestone) => {
       let proposals = getProposals(getState());
       let proposalInfo = getProposalDetails(getState());
       const proposalIndex = proposals.findIndex(
-        proposal => proposal['opportunity number'] === oppNo
+        proposal => proposal['opportunity number'] === oppNumber
       );
       if (proposalIndex > -1) {
         proposals[proposalIndex]['nextMilestone'] = nextMilestone;
