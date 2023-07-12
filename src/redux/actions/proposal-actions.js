5fd3e366a0534e3a5c1b29895a846c936b3241a1
Sushil Munda <sushil.munda@iqvia.com>
2023-07-12 10:08:21 +0530

    fix for custom name and milestone in fav tab 4211

--- src/redux/actions/proposal-actions.js
+++ src/redux/actions/proposal-actions.js
@@ -34,7 +34,8 @@ import {
   getQuestionsFilters,
   selectProposalQuestions,
   getProposals,
-  getProposalDetails
+  getProposalDetails,
+  getFavouriteProposals
 } from '../selectors';
 import { getSelectedBid, getUniqueMilestones } from '../selectors/proposal';
 import { getErrorMessage, getProposalIdlist } from '../../utils/utils';
@@ -126,7 +127,7 @@ const {
   DASHBOARD_PROPOSAL_DETAIL
 } = REDUX_TYPES.PROPOSAL;
 
-const { ON_GET_PROPOSALS } = REDUX_TYPES.PROPOSALS;
+const { ON_GET_PROPOSALS, ON_GET_FAVOURITE } = REDUX_TYPES.PROPOSALS;
 const { SET_CUSTOM_NAME_MAP } = REDUX_TYPES.SSO_AUTH;
 
 /**
@@ -1788,13 +1789,23 @@ export const onSaveCustomName = (oppNo, customName) => {
       });
       let proposals = getProposals(getState());
       let proposalInfo = getProposalDetails(getState());
+      let proposalsFavourite = getFavouriteProposals(getState());
       const proposalIndex = proposals.findIndex(
         proposal => proposal['opportunity number'] === oppNo
-      );
+      ); 
       if (proposalIndex > -1) {
         proposals[proposalIndex]['customName'] = customName;
         dispatch({ type: ON_GET_PROPOSALS, payload: { proposals } });
       }
+
+      const favouriteIndex = proposalsFavourite.findIndex(
+        proposal => proposal['opportunity number'] === oppNo
+      );
+      if (favouriteIndex > -1) {
+        proposalsFavourite[favouriteIndex]['customName'] = customName;
+        dispatch({ type: ON_GET_FAVOURITE, payload: { proposalsFavourite } });
+      }
+
       if (proposalInfo['CRM #'] == oppNo) {
         dispatch({
           type: SET_CUSTOM_NAME,
@@ -1813,6 +1824,7 @@ export const updateNextMilestone = (oppNumber, nextMilestone) => {
     try {
       let proposals = getProposals(getState());
       let proposalInfo = getProposalDetails(getState());
+      let proposalsFavourite = getFavouriteProposals(getState());
       const proposalIndex = proposals.findIndex(
         proposal => proposal['opportunity number'] === oppNumber
       );
@@ -1820,6 +1832,15 @@ export const updateNextMilestone = (oppNumber, nextMilestone) => {
         proposals[proposalIndex]['nextMilestone'] = nextMilestone;
         dispatch({ type: ON_GET_PROPOSALS, payload: { proposals } });
       }
+
+      const favouriteIndex = proposalsFavourite.findIndex(
+        proposal => proposal['opportunity number'] === oppNumber
+      );
+      if (favouriteIndex > -1) {
+        proposalsFavourite[favouriteIndex]['nextMilestone'] = nextMilestone;
+        dispatch({ type: ON_GET_FAVOURITE, payload: { proposalsFavourite } });
+      }
+
       if (proposalInfo['CRM #'] === oppNumber) {
         dispatch({
           type: SET_NEXT_MILESTONE,
@@ -1836,6 +1857,7 @@ export const updateCustomNameAction = (oppNo, customName) => {
   return async (dispatch, getState) => {
     let proposals = getProposals(getState());
     let proposalInfo = getProposalDetails(getState());
+    let proposalsFavourite = getFavouriteProposals(getState());
     let customNameMap = selectCustomNameMap(getState()).toJS();
     customNameMap[oppNo] = customName;
     dispatch({ type: SET_CUSTOM_NAME_MAP, payload: cloneDeep(customNameMap) });
@@ -1846,6 +1868,15 @@ export const updateCustomNameAction = (oppNo, customName) => {
       proposals[proposalIndex]['customName'] = customName;
       dispatch({ type: ON_GET_PROPOSALS, payload: { proposals } });
     }
+
+    const favouriteIndex = proposalsFavourite.findIndex(
+      proposal => proposal['opportunity number'] === oppNo
+    );
+    if (favouriteIndex > -1) {
+      proposalsFavourite[favouriteIndex]['customName'] = customName;
+      dispatch({ type: ON_GET_FAVOURITE, payload: { proposalsFavourite } });
+    }
+    
     if (proposalInfo['CRM #'] == oppNo) {
       dispatch({
         type: SET_CUSTOM_NAME,
