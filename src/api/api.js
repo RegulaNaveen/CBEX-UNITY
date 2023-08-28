45dcfe4d0410079b1eb9150fcfa5631ac15e8cfa
Kunal Nigam <kunal.nigam@iqvia.com>
2023-08-28 09:39:31 +0000

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -59,8 +59,8 @@ switch (environment) {
     break;
   default:
     // DEV Data
-    PROPOSAL_API_ENDPOINT = 'http://localhost:5000';
-    // 'https://olyxc9cn1m.execute-api.us-east-1.amazonaws.com/dev';
+    PROPOSAL_API_ENDPOINT =
+      'https://olyxc9cn1m.execute-api.us-east-1.amazonaws.com/dev';
     PROPOSAL_API_KEY = 'SmXooYpNRX4u0dlRrebjt3PUnJJma5cm5ipK3nSV';
     NORMAL_AUTH_API_ENDPOINT =
       'https://byp6zagvkb.execute-api.us-east-1.amazonaws.com/unity-dev';
