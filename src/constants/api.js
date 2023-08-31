c510be61e3a8da9aa4b11960a7905e14d85b0d43
Sushil Munda <sushil.munda@iqvia.com>
2023-08-31 15:16:10 +0530

    added periods for update template modal

--- src/constants/api.js
+++ src/constants/api.js
@@ -176,8 +176,8 @@ switch (environment) {
   case 'DEV':
     // DEV Data
     PROPOSAL_API_ENDPOINT =
-      // 'http://localhost:5000';
-      'https://olyxc9cn1m.execute-api.us-east-1.amazonaws.com/dev';
+      'http://localhost:5000';
+      // 'https://olyxc9cn1m.execute-api.us-east-1.amazonaws.com/dev';
     PROPOSAL_API_ENDPOINT_2 =
       'https://a9cm724u04.execute-api.us-east-1.amazonaws.com/dev';
     PROPOSAL_API_KEY = 'SmXooYpNRX4u0dlRrebjt3PUnJJma5cm5ipK3nSV';
@@ -185,8 +185,8 @@ switch (environment) {
       'https://byp6zagvkb.execute-api.us-east-1.amazonaws.com/unity-dev';
     COGNITO_HOST = 'https://dev-unity.auth.us-east-1.amazoncognito.com';
     CLIENT_ID = 'uf2jbhv4jcprcdiqc5rupg665';
-    REDIRECTION_URL = 'https://dev-unity.dev.iqvia.app/';
-    // REDIRECTION_URL = 'http://localhost:8080';
+    // REDIRECTION_URL = 'https://dev-unity.dev.iqvia.app/';
+    REDIRECTION_URL = 'http://localhost:8080';
 
     UBUILD_ARTIFACT = 'https://dev-ubuild.dev.iqvia.app/main.js';
     SF_HOST_URL = 'https://iqvia--crm.lightning.force.com/';
