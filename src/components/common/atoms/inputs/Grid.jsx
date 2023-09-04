1182db3e910d97e95204a1dc43eff929d769eb35
Sushil Munda <sushil.munda@iqvia.com>
2023-09-04 15:28:11 +0530

    bug fix 4975 header bidtype concurrency issue

--- src/components/common/atoms/inputs/Grid.jsx
+++ src/components/common/atoms/inputs/Grid.jsx
@@ -87,8 +87,9 @@ const loadSidebar = props => {
   const dispatch = useDispatch();
   const { updateFavouriteWrapper } = useContext(SocketContext);
   const updatedProposalDetail = useSelector(state => state?.proposal);
-  const selectedBid = useSelector(getSelectedBid);
-  const bidType = selectedBid.get('bidType');
+  // const selectedBid = useSelector(getSelectedBid);
+  // const bidType = selectedBid.get('bidType');
+  // console.log('bidType', bidType);
 
   const {
     'Bid due date': bidDueDate,
@@ -101,9 +102,10 @@ const loadSidebar = props => {
     'CRM #': crm,
     Customer,
     bidNo,
+    bidType,
     opportunityId
   } = data;
-  const placeholder = 'No data';
+    const placeholder = 'No data';
   const date = bidDueDate && parseMomentDate(bidDueDate);
   const daysRemain = getRemainingDays(date);
   const redirect = () => {
@@ -494,7 +496,9 @@ const loadSidebar = props => {
             Bid #
           </Typography>
           <Typography variant="body2" className="boldtext">
-            {bidType === 'Early_Engagement_Bid' ? 'Early Engagement  ' : ''}
+            {bidType === 'Early_Engagement_Bid' 
+              || bidType === 'Bid Early_Engagement_Bid' 
+              ? 'Early Engagement  ' : ''}
             {bidNo || placeholder}
           </Typography>
         </Paper>
