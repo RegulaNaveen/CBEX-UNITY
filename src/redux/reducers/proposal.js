97cd7d5dee0268327376fba22e35937bd9a03906
Varsha <varsha.kumari2@iqvia.com>
2023-12-05 11:18:58 +0530

    fix for tooltip and approvals

--- src/redux/reducers/proposal.js
+++ src/redux/reducers/proposal.js
@@ -145,7 +145,7 @@ const INITIAL_STATE: Map = fromJS({
       },
       showInactiveQuestions: {
         checked: false,
-        label: 'Include N/A Questions',
+        label: 'Include Not Applicable Questions',
         className: 'questions-filter__row3-col1'
       },
       logic: 'OR'
