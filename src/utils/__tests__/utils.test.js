ca737c7cc8400d674b68c7a1e1c92455fa28d4a7
Chahar <pooja.chahar@iqvia.com>
2023-12-07 10:09:23 +0530

    unit test case for post award and rfi bid

--- src/utils/__tests__/utils.test.js
+++ src/utils/__tests__/utils.test.js
@@ -14,4 +14,12 @@ describe('utils.js tests', () => {
     const bidName = getBidNameByType('Clinical_Bid');
     expect(bidName).toBe('Bid');
   });
+  test("getBidNameByType('Post_Award_Bid') should return 'Post Award'", () => {
+    const bidName = getBidNameByType('Post_Award_Bid');
+    expect(bidName).toBe('Post Award ');
+  });
+  test("getBidNameByType('RFI_Request') should return 'RFI'", () => {
+    const bidName = getBidNameByType('RFI_Request');
+    expect(bidName).toBe('RFI ');
+  });
 });
