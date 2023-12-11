21b8fbb6f28d810516a7d0a163ce29b208fe98df
Sushil Munda <sushil.munda@iqvia.com>
2023-12-11 12:48:21 +0530

    revert the change

--- src/components/screens/UnityTabs/SectionActive.jsx
+++ src/components/screens/UnityTabs/SectionActive.jsx
@@ -25,9 +25,9 @@ const SectionActive = ({
 }) => {
   const allTab = useSelector(state => state.unitytab.allTabs);
   let tab = allTab[tabId];
-  const { isCurrent, isEditable } = useSelector(getSelectedBid)?.toJS();
+  const { isCurrent } = useSelector(getSelectedBid)?.toJS();
 
-  const selectedBidIsCurrent = !!isCurrent || !!isEditable;
+  const selectedBidIsCurrent = !!isCurrent;
   const [questionVisibility, setQuestionVisibility] = useState({});
   const [currentsection, setCurrentSection] = useState('');
   const [showModal, setShowModal] = useState(false);
