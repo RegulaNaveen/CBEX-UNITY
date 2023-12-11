6164986213924e6531f74ff4ba4e731ce5a02797
Sushil Munda <sushil.munda@iqvia.com>
2023-12-11 12:15:57 +0530

    fix for most recent bid type editbale in approval and custom tab

--- src/components/screens/UnityTabs/SectionActive.jsx
+++ src/components/screens/UnityTabs/SectionActive.jsx
@@ -25,9 +25,9 @@ const SectionActive = ({
 }) => {
   const allTab = useSelector(state => state.unitytab.allTabs);
   let tab = allTab[tabId];
-  const { isCurrent } = useSelector(getSelectedBid)?.toJS();
+  const { isCurrent, isEditable } = useSelector(getSelectedBid)?.toJS();
 
-  const selectedBidIsCurrent = !!isCurrent;
+  const selectedBidIsCurrent = !!isCurrent || !!isEditable;
   const [questionVisibility, setQuestionVisibility] = useState({});
   const [currentsection, setCurrentSection] = useState('');
   const [showModal, setShowModal] = useState(false);
