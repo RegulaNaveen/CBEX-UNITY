30f66abffc0015f250ad07f2bb4587551681335d
Aggarwal <Akash.Aggarwal@iqvia.com>
2023-12-14 13:33:30 +0530

    copy public in dist folder

--- src/components/views/WysiwygNotepad/MenuItem.jsx
+++ src/components/views/WysiwygNotepad/MenuItem.jsx
@@ -8,7 +8,9 @@ export default ({ icon, title, action, isActive = null }) => (
     style={{ height: '25px', width: '25px' }}
   >
     <svg className="remix">
-      <use xlinkHref={`%PUBLIC_URL%/assets/symbol.svg#ri-${icon}`} />
+      <use
+        xlinkHref={`${window.location.origin}/assets/symbol.svg#ri-${icon}`}
+      />
     </svg>
   </button>
 );
