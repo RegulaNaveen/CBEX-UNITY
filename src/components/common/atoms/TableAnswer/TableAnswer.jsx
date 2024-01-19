5fd1ef1ee1d8b2a35c267f640e0d233ca423133c
Sushil Munda <sushil.munda@iqvia.com>
2024-01-19 16:58:42 +0530

    fix for auto focus in input field and duplicate msg 5794

--- src/components/common/atoms/TableAnswer/TableAnswer.jsx
+++ src/components/common/atoms/TableAnswer/TableAnswer.jsx
@@ -302,6 +302,9 @@ function TableAnswer({
         behaviour: 'smooth',
         inline: 'end'
       });
+      const thead =
+        tableRef.current.horizontalScrollRef.current.lastChild.firstChild;
+      thead.firstChild.lastChild.firstChild.firstChild.children[1].firstChild.focus();
     }, 700);
     if (onCascadeChange) onCascadeChange();
   }
@@ -363,6 +366,14 @@ function TableAnswer({
         behaviour: 'smooth',
         block: 'end'
       });
+      const tBody =
+        tableRef.current.horizontalScrollRef.current.lastChild.lastChild;
+      if (tBody.lastChild.firstChild.firstChild)
+        tBody.lastChild.firstChild.firstChild.firstChild.children[1].firstChild.focus();
+      else
+        tBody.children[
+          tBody.children.length - 2
+        ].firstChild.firstChild.firstChild.children[1].firstChild.focus();
     }, 700);
     if (onCascadeChange) onCascadeChange();
   }
@@ -429,20 +440,22 @@ function TableAnswer({
       setWarning(true);
       setWarningTitle('Alert');
       setWarningText(
-        duplicateRows.length > 0 && duplicateColumns.length > 0
-          ? `${TABLEANSWER.DUPLICATE_ROWS} ${removeDuplicates(
-              duplicateRows
-            ).join(', ')}
-              ${TABLEANSWER.DUPLICATE_COLUMNS} ${removeDuplicates(
-              duplicateColumns
-            ).join(', ')}`
-          : duplicateRows.length > 0
-          ? `${TABLEANSWER.DUPLICATE_ROWS} ${removeDuplicates(
-              duplicateRows
-            ).join(', ')}`
-          : `${TABLEANSWER.DUPLICATE_COLUMNS} ${removeDuplicates(
-              duplicateColumns
-            ).join(', ')}`
+        duplicateRows.length > 0 && duplicateColumns.length > 0 ? (
+          <Typography>
+            {TABLEANSWER.DUPLICATE_ROWS}{' '}
+            {removeDuplicates(duplicateRows).join(', ')}. <br />
+            {TABLEANSWER.DUPLICATE_COLUMNS}{' '}
+            {removeDuplicates(duplicateColumns).join(', ')}.
+          </Typography>
+        ) : duplicateRows.length > 0 ? (
+          `${TABLEANSWER.DUPLICATE_ROWS} ${removeDuplicates(duplicateRows).join(
+            ', '
+          )}.`
+        ) : (
+          `${TABLEANSWER.DUPLICATE_COLUMNS} ${removeDuplicates(
+            duplicateColumns
+          ).join(', ')}.`
+        )
       );
     }
   }
@@ -535,7 +548,7 @@ function TableAnswer({
         let longesColIndex = 0;
         let longestColHeader = cloneColumns[0].headerTitle;
         cloneColumns.forEach((column, index) => {
-          if (column.headerTitle.length > longestColHeader.length) {
+          if (column?.headerTitle?.length > longestColHeader?.length) {
             longesColIndex = index;
             longestColHeader = column.headerTitle;
           }
