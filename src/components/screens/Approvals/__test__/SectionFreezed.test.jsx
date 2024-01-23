ec2f7c40b41cdb5cc35bfa31c4761fcfa3542359
Chahar <pooja.chahar@iqvia.com>
2024-01-23 19:27:13 +0530

    unit test case for section Freezed and custom loader

--- src/components/screens/Approvals/__test__/SectionFreezed.test.jsx
+++ src/components/screens/Approvals/__test__/SectionFreezed.test.jsx
@@ -35,5 +35,37 @@ describe('SectionFreezed', () => {
         </SocketContext.Provider>
       </Provider>
     );
+    expect(container).toBeInTheDocument();
+  });
+
+  test('render the component without crashing with props 1', async () => {
+    const { container, getByTestId } = await render(
+      <Provider store={store}>
+        <SocketContext.Provider
+          value={{
+            questionLockWrapper: jest.fn(),
+            questionUnlockWrapper: jest.fn()
+          }}
+        >
+          <SectionFreezed
+            ArchivedData={[]}
+            ApprovalSectionTitle="Test-5"
+            ApprovalSectionRightQuestions={
+              ('8e89a496-c3c5-47ce-b193-b7406fbf9ae1',
+              '8ae5c1e8-5c02-4eb2-a9ea-e2f54593f2e3',
+              '231be643-6814-4059-93bf-c9fb21b658f6')
+            }
+            ApprovalSectionOrder="15"
+            ApprovalSectionId="f60eb2da-70a7-41e5-9fb3-37e02a6194d5"
+            ApprovalSectionLeftQuestions={
+              ('8e89a496-c3c5-47ce-b193-b7406fbf9ae1',
+              '8ae5c1e8-5c02-4eb2-a9ea-e2f54593f2e3',
+              '231be643-6814-4059-93bf-c9fb21b658f6')
+            }
+          />
+        </SocketContext.Provider>
+      </Provider>
+    );
+    expect(container).toBeInTheDocument();
   });
 });
