a2f6ceae8fdbed5b1e2ccb429f93cfdd5fb811b4
Chahar <pooja.chahar@iqvia.com>
2024-02-03 18:22:13 +0530

    add search functionality

--- src/components/common/atoms/TableAnswer/TablePreview.jsx
+++ src/components/common/atoms/TableAnswer/TablePreview.jsx
@@ -1,9 +1,64 @@
 import React, { useEffect, useState, useRef } from 'react';
 import Tooltip from 'apollo-react/components/Tooltip';
+import { useDispatch, useSelector } from 'react-redux';
+import Highlighter from 'react-highlight-words';
+import {
+  selectQuery,
+  selectCurrentSearchResult,
+  selectAutoNavigatedToCurrentResult
+} from '../../../../redux/selectors/search';
+import { autoNavigationCompletedAction } from '../../../../redux/actions/search-actions';
 
 export default function TablePreview({ columns, rows }) {
   var totalNotHiddenColumns = columns.filter(column => !column.hidden);
   var totalNotHiddenRows = rows.filter(row => !row.hidden);
+  const tableColumnRef = useRef(null);
+  const tableRowRef = useRef(null);
+  const dispatch = useDispatch();
+  const query = useSelector(selectQuery);
+  const currentSearchResult = useSelector(selectCurrentSearchResult);
+  const autoNavigatedToCurrentResult = useSelector(
+    selectAutoNavigatedToCurrentResult
+  );
+
+  useEffect(() => {
+    if (
+      currentSearchResult !== null &&
+      tableColumnRef.current !== null &&
+      !autoNavigatedToCurrentResult
+    ) {
+      totalNotHiddenColumns.map((column, index) => {
+        if (currentSearchResult.inputText === column.headerTitle) {
+          setTimeout(() => {
+            tableColumnRef.current.scrollIntoView({
+              behavior: 'auto',
+              block: 'end',
+              inline: 'start'
+            });
+            dispatch(autoNavigationCompletedAction());
+          }, 700);
+        }
+      });
+      totalNotHiddenRows.map(row => {
+        totalNotHiddenColumns.map((column, index) => {
+          if (currentSearchResult.inputText === row[column.accessor]) {
+            setTimeout(() => {
+              tableRowRef.current.scrollIntoView({
+                behavior: 'auto',
+                block: 'end',
+                inline: 'start'
+              });
+              dispatch(autoNavigationCompletedAction());
+            }, 700);
+          }
+        });
+      });
+    }
+  }, [
+    currentSearchResult,
+    tableColumnRef.current,
+    autoNavigatedToCurrentResult
+  ]);
 
   return (
     <div className="custom-answer-table-container">
@@ -18,7 +73,23 @@ export default function TablePreview({ columns, rows }) {
                     placement="top"
                     id="table-tooltip"
                   >
-                    <p>{column.headerTitle}</p>
+                    <p ref={tableColumnRef}>
+                      <Highlighter
+                        searchWords={[
+                          `${
+                            currentSearchResult !== null &&
+                            currentSearchResult.inputText ===
+                              column.headerTitle &&
+                            query !== null
+                              ? query
+                              : ''
+                          }`
+                        ]}
+                        autoEscape={true}
+                        textToHighlight={column.headerTitle}
+                        highlightClassName="search-highlight"
+                      />
+                    </p>
                   </Tooltip>
                 </th>
               ))}
@@ -38,7 +109,23 @@ export default function TablePreview({ columns, rows }) {
                             placement="top"
                             id="table-tooltip"
                           >
-                            <p>{row[column.accessor]}</p>
+                            <p ref={tableRowRef}>
+                              <Highlighter
+                                searchWords={[
+                                  `${
+                                    currentSearchResult !== null &&
+                                    currentSearchResult.inputText ===
+                                      row[column.accessor] &&
+                                    query !== null
+                                      ? query
+                                      : ''
+                                  }`
+                                ]}
+                                autoEscape={true}
+                                textToHighlight={row[column.accessor]}
+                                highlightClassName="search-highlight"
+                              />
+                            </p>
                           </Tooltip>
                         </td>
                       </>
