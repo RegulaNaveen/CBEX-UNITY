1bc147a1592267c8a3fa65f94e9d6d09d75c8583
Keerthiprasath <keerthiprasath.chandran@iqvia.com>
2024-02-07 11:11:33 +0530

    [feature/IQVIACBEXU-5813] mocked GenerateDocs.jsx

--- src/__tests__/index.test.jsx
+++ src/__tests__/index.test.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { mount } from 'enzyme';
 import App from '../App';
 
+jest.mock('../components/views/export-component/GenerateDocs.jsx', () => (
+  <p>React PDF Component</p>
+));
+
 describe('App', () => {
   it.skip('renders without crashing', () => {
     const div = document.createElement('div');
