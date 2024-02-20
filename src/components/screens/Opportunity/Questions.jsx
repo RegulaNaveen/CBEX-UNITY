7f4dc5c65614a12d8536fd970ac458b70cd6b806
Varsha <varsha.kumari2@iqvia.com>
2024-02-20 17:20:13 +0530

    files removed

--- src/components/screens/Opportunity/Questions.jsx
+++ src/components/screens/Opportunity/Questions.jsx
@@ -4,7 +4,7 @@
 // eslint-disable-next-line react/destructuring-assignment
 import React, { createRef, createContext, Component, Suspense } from 'react';
 import { withRouter, Match } from 'react-router-dom';
-import { List } from 'immutable';
+import { List, Map } from 'immutable';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import Button from 'apollo-react/components/Button';
