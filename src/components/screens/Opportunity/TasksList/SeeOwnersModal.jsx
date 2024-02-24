169965a9d345ba5f9e185953cc6e7548f704d196
Sushil Munda <sushil.munda@iqvia.com>
2024-02-24 16:13:29 +0530

    unit test cases for see owners in task list

--- src/components/screens/Opportunity/TasksList/SeeOwnersModal.jsx
+++ src/components/screens/Opportunity/TasksList/SeeOwnersModal.jsx
@@ -349,7 +349,11 @@ const SeeOwners = ({
       {isModalOpen && (
         <div id="modal-overlay">
           <div className="modal">
-            <button className="close-button" onClick={handleClose}>
+            <button
+              className="close-button"
+              onClick={handleClose}
+              data-testid="close-button"
+            >
               X
             </button>
             <div className="modal-content">
@@ -393,7 +397,10 @@ const SeeOwners = ({
                             className="remove-user"
                             onClick={() => handleRemoveUser(index)}
                           >
-                            <TrashIcon fontSize="small" />
+                            <TrashIcon
+                              fontSize="small"
+                              data-testid="trash-icon"
+                            />
                           </div>
                         </div>
                       )
--- src/components/screens/Opportunity/TasksList/__tests__/index.test.jsx
+++ src/components/screens/Opportunity/TasksList/__tests__/index.test.jsx
@@ -8,6 +8,7 @@ import mockData from '../../../../views/__tests__/Search/data.json';
 import { TASKS } from '../../../../../constants/types';
 import moment from 'moment';
 import { SocketContext } from '../../../../../context/SocketContext';
+import data from '../../../../views/modals/__test__/data.json';
 
 const { LOADING_TASKS, SET_TASKS } = TASKS;
 
@@ -36,7 +37,38 @@ describe('TasksList Unit Tests', () => {
           no_of_units: 1,
           description: 'task 1',
           order: 1,
-          opportunity_types: 'Default Type'
+          opportunity_types: 'Default Type',
+          task_role: [
+            {
+              task_list_id: 20,
+              proposal_id: 'a49ed80a-d782-40c0-9ff4-bbe35eb0e904',
+              task_id: '4d1b5820-cd8b-417e-8b64-5a8bfe91b842',
+              question_id: null,
+              name: 'RAHUL TIWARI',
+              email: 'rahul.tiwari@iqvia.com',
+              type: 'user'
+            },
+            {
+              id: 1390,
+              task_list_id: 20,
+              proposal_id: 'a49ed80a-d782-40c0-9ff4-bbe35eb0e904',
+              task_id: '4d1b5820-cd8b-417e-8b64-5a8bfe91b842',
+              question_id: null,
+              name: 'Varsha Agarwal',
+              email: 'varsha.goyal@iqvia.com',
+              type: 'user'
+            },
+            {
+              id: 24,
+              task_list_id: 20,
+              proposal_id: 'a49ed80a-d782-40c0-9ff4-bbe35eb0e904',
+              task_id: '4d1b5820-cd8b-417e-8b64-5a8bfe91b842',
+              question_id: 'd7f00e27-409d-4220-af5d-a015f189a5f5',
+              name: null,
+              email: null,
+              type: 'roles'
+            }
+          ]
         },
         {
           no_of_units: 1,
@@ -66,7 +98,8 @@ describe('TasksList Unit Tests', () => {
           proposal: {
             ...mockData.proposal,
             proposalDate: moment('2024-02-16').subtract(1, 'days')
-          }
+          },
+          proposalQuestions: [data.proposalTeamQuestion]
         },
         bid: {
           bidId: '',
@@ -153,6 +186,104 @@ describe('TasksList Unit Tests', () => {
     await waitFor(() => {
       expect(getByText('task 1')).toBeInTheDocument();
     });
+  });
+
+  test('see owners of task', async () => {
+    render(<TasksListWithRedux />);
+    await waitFor(() => {
+      expect(screen.getByText('task 1')).toBeInTheDocument();
+    });
+    const expandToggle = screen.getByTestId('ellipsis-vertical-1');
+    fireEvent.click(expandToggle);
+    const seeOwners = screen.getByText('See Owners()');
+    fireEvent.click(seeOwners);
+    expect(screen.getByText('Task Owners')).toBeInTheDocument();
+
+    const closeButton = screen.getByTestId('close-button');
+    fireEvent.click(closeButton);
+  });
+
+  test('see owners of task || add new owner', async () => {
+    render(<TasksListWithRedux />);
+    await waitFor(() => {
+      expect(screen.getByText('task 1')).toBeInTheDocument();
+    });
+    const expandToggle = screen.getByTestId('ellipsis-vertical-1');
+    fireEvent.click(expandToggle);
+    const seeOwners = screen.getByText('See Owners()');
+    fireEvent.click(seeOwners);
+    expect(screen.getByText('Task Owners')).toBeInTheDocument();
+
+    const addOwner = screen.getByText('Add Owner');
+    fireEvent.click(addOwner);
+    expect(screen.getByText('Add Owner')).toBeInTheDocument();
+
+    const emailInput = screen.getByPlaceholderText('Add user');
+    fireEvent.change(emailInput, { target: { value: 'owner' } });
+
+    fetch = jest.fn().mockResolvedValue(
+      Promise.resolve({
+        json: () =>
+          Promise.resolve({
+            data: [
+              {
+                first_name: 'new',
+                last_name: 'owner',
+                email: 'newowner@test.com'
+              }
+            ]
+          })
+      })
+    );
+
+    fireEvent.change(emailInput, { target: { value: 'new owner' } });
+    await waitFor(() => {
+      // fireEvent.click(screen.getByText('new owner(newowner@test.com)'));
+    });
+  }, 7000);
+
+  test('see owners of task || with owner', async () => {
+    render(<TasksListWithRedux />);
+    await waitFor(() => {
+      expect(screen.getByText('task 1')).toBeInTheDocument();
+    });
+    const expandToggle = screen.getByTestId('ellipsis-vertical-1');
+    fireEvent.click(expandToggle);
+    const seeOwners = screen.getByText('See Owners()');
+    fireEvent.click(seeOwners);
+    expect(screen.getByText('Task Owners')).toBeInTheDocument();
+    expect(screen.getByText('Varsha Agarwal')).toBeInTheDocument();
+
+    // Delete a task owner
+    const deleteOwner = screen.getAllByTestId('trash-icon');
+    fireEvent.click(deleteOwner[0]);
+    fireEvent.click(deleteOwner[1]);
+
+    fireEvent.click(screen.getByText('Cancel'));
+  });
+
+  test('see owners of task || with proposal team owner', async () => {
+    render(<TasksListWithRedux />);
+    await waitFor(() => {
+      expect(screen.getByText('task 1')).toBeInTheDocument();
+    });
+    const expandToggle = screen.getByTestId('ellipsis-vertical-1');
+    fireEvent.click(expandToggle);
+    const seeOwners = screen.getByText('See Owners()');
+    fireEvent.click(seeOwners);
+    expect(screen.getByText('Task Owners')).toBeInTheDocument();
+    expect(screen.getByText('Sushil Munda')).toBeInTheDocument();
+
+    // Delete a task owner
+    const deleteOwner = screen.getAllByTestId('trash-icon');
+    fireEvent.click(deleteOwner[2]);
+
+    expect(
+      screen.getByText(
+        'This Task will no longer reflect the settings in the Teams section'
+      )
+    ).toBeVisible();
+    fireEvent.click(screen.getByText('Continue'));
   });
 
   test('drag and drop task', async () => {
