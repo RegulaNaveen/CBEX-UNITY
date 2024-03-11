ebf8c8b4c46f42eae8b0732aa477d427ad4a219b
Keerthiprasath <keerthiprasath.chandran@iqvia.com>
2024-03-11 15:43:31 +0530

    bugfix - notepad search issue with autoscroll

--- src/components/views/WysiwygNotepad/index.jsx
+++ src/components/views/WysiwygNotepad/index.jsx
@@ -76,7 +76,9 @@ const WysiwygNotepad = ({
           if (!localStorage.getItem('notepadStartDuration')) {
             localStorage.setItem(
               'notepadStartDuration',
-              moment().utc().format('MMMM Do YYYY, h:mm:ss a')
+              moment()
+                .utc()
+                .format('MMMM Do YYYY, h:mm:ss a')
             );
           }
         });
@@ -175,7 +177,9 @@ const WysiwygNotepad = ({
           matamoObj.action = `Event: Notepad ${proposalDetails['CRM #']}`;
           matamoObj.name = `Notepad: Duration ${localStorage.getItem(
             'notepadStartDuration'
-          )} - ${moment().utc().format('MMMM Do YYYY, h:mm:ss a')}`;
+          )} - ${moment()
+            .utc()
+            .format('MMMM Do YYYY, h:mm:ss a')}`;
           matamoObj.customDimensions = [
             JSON.stringify(proposalDetails),
             { user: userEmail },
@@ -483,10 +487,12 @@ const WysiwygNotepad = ({
     ) {
       if (query !== null && query.length >= 3 && dataSynced) {
         !editor.isDestroyed &&
-          editor.commands.search(
-            query !== null ? query : '',
-            currentSearchResult.matchIndex
-          );
+          setTimeout(() => {
+            editor.commands.search(
+              query !== null ? query : '',
+              currentSearchResult.matchIndex
+            );
+          }, 1000);
       }
     } else if (
       editor &&
