9c71dabc0c15eaf6974b26fe11157c309ced699e
Chanchal Kumar <chanchal.kumar@iqvia.com>
2024-07-04 11:13:59 +0530

    bubble thickness fix

--- src/components/views/ChatBot/MultiResponseChat.jsx
+++ src/components/views/ChatBot/MultiResponseChat.jsx
@@ -14,57 +14,54 @@ export const MultiResponseChat = ({
   handleReviewDoc = () => {}
 }) => {
   return (
-    <div className={className}>
-      <div className="multiresponse-list">
-        {list?.map((response, i) => (
-          <div>
-            {' '}
-            <p>
-              {list.length > 1 && `${i + 1}. `}
-              {response?.result}
-              <div className="src-doc-list">
-                {response?.source_documents
-                  ?.filter(obj => obj.type == 'Document')
-                  .map((sourceDoc, i) => (
-                    <SourceDocument
-                      title={getSourceDocNameFromPath(
-                        sourceDoc.metadata.source
-                      )}
-                      content={sourceDoc.page_content}
-                      buttonLabel={`[${i + 1}]`}
-                      className="source-doc-btn"
-                    />
-                  ))}
-              </div>
-            </p>
-            {response?.source_documents?.map((sourceDoc, i) => {
-              if (sourceDoc?.type != 'Document' && sourceDoc?.type != 'Unity') {
-                return null;
-              }
-              return (
-                <Button
-                  variant="secondary"
-                  size="small"
-                  className="chatbot-action-btn"
-                  onClick={() => {
-                    if (sourceDoc.type == 'Document') {
-                      handleReviewDoc(sourceDoc.metadata.box_file_id);
-                    } else if (sourceDoc.type == 'Unity') {
-                      handleGotoQuestion(sourceDoc.bid_no, sourceDoc.content);
-                    }
-                  }}
-                >
-                  {sourceDoc.type == 'Document'
-                    ? `Review ${getSourceDocNameFromPath(
-                        sourceDoc.metadata.source
-                      )}`
-                    : 'Go to Unity Question'}
-                </Button>
-              );
-            })}
-          </div>
-        ))}
-      </div>
+    <div className="multiresponse-list">
+      {list?.map((response, i) => (
+        <div>
+          {' '}
+          <p>
+            {list.length > 1 && `${i + 1}. `}
+            {response?.result}
+            <div className="src-doc-list">
+              {response?.source_documents
+                ?.filter(obj => obj.type == 'Document')
+                .map((sourceDoc, i) => (
+                  <SourceDocument
+                    title={getSourceDocNameFromPath(sourceDoc.metadata.source)}
+                    content={sourceDoc.page_content}
+                    buttonLabel={`[${i + 1}]`}
+                    className="source-doc-btn"
+                    pageNo={sourceDoc?.metadata?.page}
+                  />
+                ))}
+            </div>
+          </p>
+          {response?.source_documents?.map((sourceDoc, i) => {
+            if (sourceDoc?.type != 'Document' && sourceDoc?.type != 'Unity') {
+              return null;
+            }
+            return (
+              <Button
+                variant="secondary"
+                size="small"
+                className="chatbot-action-btn"
+                onClick={() => {
+                  if (sourceDoc.type == 'Document') {
+                    handleReviewDoc(sourceDoc.metadata.box_file_id);
+                  } else if (sourceDoc.type == 'Unity') {
+                    handleGotoQuestion(sourceDoc.bid_no, sourceDoc.content);
+                  }
+                }}
+              >
+                {sourceDoc.type == 'Document'
+                  ? `Review ${getSourceDocNameFromPath(
+                      sourceDoc.metadata.source
+                    )}`
+                  : 'Go to Unity Question'}
+              </Button>
+            );
+          })}
+        </div>
+      ))}
     </div>
   );
 };
