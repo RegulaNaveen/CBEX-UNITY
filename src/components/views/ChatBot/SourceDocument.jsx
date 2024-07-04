e4ec0ef1dc819a60127a809197f0a1776ce2a567
Chanchal Kumar <chanchal.kumar@iqvia.com>
2024-07-04 12:00:39 +0530

    tooltip-title margin added & pageNo added

--- src/components/views/ChatBot/SourceDocument.jsx
+++ src/components/views/ChatBot/SourceDocument.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import Tooltip from 'apollo-react/components/Tooltip';
 
-const SourceDocument = ({ className = '', buttonLabel, title, content }) => (
+const SourceDocument = ({
+  className = '',
+  buttonLabel,
+  title,
+  content,
+  pageNo
+}) => (
   <div className={className}>
     <Tooltip
       id="answer-tooltip"
       variant="light"
-      title={title}
+      title={
+        <div className="tooltip-title">
+          <div>{title.replace(/^\/usr\/src\/app\/api\/data\//, '')}</div>
+          <div>Page: {pageNo}</div>
+        </div>
+      }
       subtitle={content}
       placement="left-start"
     >
