2d9e79c9da09361445cdb6db2be2dc603eb96d55
Naveen <regulanaveen1608@gmail.com>
2026-03-31 22:47:56 +0530

    my first commit

--- src/uiActions/ui-actions.js
+++ src/uiActions/ui-actions.js
@@ -4,3 +4,5 @@ export const UI_ACTION = {
   openGenerateModal: 'openGenerateModal'
 };
 export const actionChannel = new Subject();
+export const actionChannel = new Subject();
+export const actionChannel = new Subject();
