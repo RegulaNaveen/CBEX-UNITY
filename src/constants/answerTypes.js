0d9b54216e7939bd4ff3b7fac8803831d6e0025b
REGULA NAVEEN <104778882+RegulaNaveen@users.noreply.github.com>
2026-04-04 03:38:41 +0530

    Remove ANSWER_TYPES constant from answerTypes.jsc

--- src/constants/answerTypes.js
+++ src/constants/answerTypes.js
@@ -1,17 +1,1 @@
-const ANSWER_TYPES = {
-  TEXT: 'text',
-  NUMBER: 'number',
-  YES_NO: 'y/n',
-  SELECT: 'select',
-  SELECT_LOOKUP: 'select-lookup',
-  DATE: 'date',
-  PICKLIST: 'picklist',
-  PICKLIST_LOOKUP: 'picklist-lookup',
-  RADIO: 'radio',
-  CHECKBOX: 'checkbox',
-  MULTI_SELECT: 'multi-select',
-  MULTI_SELECT_LOOKUP: 'multi-select-lookup',
-  TABLE: 'table',
-  CONTACT: 'contact'
-};
-export default ANSWER_TYPES;
+
